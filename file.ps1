c8f0943f2f0dd70d42c903801bc82a80c38f3118
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-23 01:23:16 +0300

    Update file.ps1

--- file.ps1
+++ file.ps1
@@ -73,3 +73,20 @@ foreach ($pattern in $wildcards) {
         } catch {}
     }
 }
+
+@(
+  "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser",
+  "Microsoft\Windows\Autochk\Proxy",
+  "Microsoft\Windows\Customer Experience Improvement Program\Consolidator",
+  "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip",
+  "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector",
+  "Microsoft\Windows\Feedback\Siuf\DmClient",
+  "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload",
+  "Microsoft\Windows\Windows Error Reporting\QueueReporting",
+  "Microsoft\Windows\Application Experience\MareBackup",
+  "Microsoft\Windows\Application Experience\StartupAppTask",
+  "Microsoft\Windows\Application Experience\PcaPatchDbTask",
+  "Microsoft\Windows\Maps\MapsUpdateTask"
+) | ForEach-Object {
+  schtasks /Change /TN $_ /Disable | Out-Null
+}
