e52a6d02366e310a3b406436ffc64c24e27d2b1d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-23 01:20:37 +0300

    Update SystemTweaks.ps1

--- SystemTweaks.ps1
+++ SystemTweaks.ps1
@@ -1,6 +1,7 @@
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value ((Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1kb) -Force
 Remove-Item "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl" -ErrorAction SilentlyContinue; icacls "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger" /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
 Curl https://raw.githubusercontent.com/GabiNun/Scripts/main/file.reg -OutFile $env:TEMP\file.reg
+Curl https://raw.githubusercontent.com/GabiNun/Scripts/main/file.reg -OutFile $env:TEMP\file.ps1
 [Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
 $ErrorActionPreference = 'SilentlyContinue'
 
@@ -19,82 +20,6 @@ $ErrorActionPreference = 'SilentlyContinue'
   "Microsoft\Windows\Maps\MapsUpdateTask"
 ) | ForEach-Object {
   schtasks /Change /TN $_ /Disable | Out-Null
-}
-
-$serviceGroups = @{
-    "Automatic" = @(
-        "AudioEndpointBuilder", "AudioSrv", "BFE", "BrokerInfrastructure", "BthAvctpSvc", "CoreMessagingRegistrar",
-        "CryptSvc", "DPS", "DcomLaunch", "Dhcp", "Dnscache", "DusmSvc", "EventLog", "EventSystem",
-        "FontCache", "IKEEXT", "KeyIso", "LSM", "LanmanWorkstation", "MpsSvc", "Netlogon", "Power",
-        "ProfSvc", "RpcEptMapper", "RpcSs", "SamSs", "Schedule", "SENS", "ShellHWDetection",
-        "Spooler", "SystemEventsBroker", "TermService", "Themes", "TrkWks", "UserManager",
-        "VaultSvc", "Wcmsvc", "WinDefend", "Winmgmt", "WlanSvc"
-    )
-    "Manual" = @(
-        "ALG", "AppIDSvc", "AppMgmt", "AppReadiness", "AppXSvc", "Appinfo", "AssignedAccessManagerSvc",
-        "AxInstSV", "BDESVC", "Browser", "CDPSvc", "COMSysApp", "CertPropSvc", "ClipSVC", "CscService",
-        "DevQueryBroker", "DeviceAssociationService", "DeviceInstall", "DisplayEnhancementService",
-        "DmEnrollmentSvc", "DsSvc", "DsmSvc", "EFS", "EapHost", "EntAppSvc", "FDResPub", "FrameServer",
-        "FrameServerMonitor", "GraphicsPerfSvc", "HvHost", "InstallService", "InventorySvc",
-        "IpxlatCfgSvc", "KtmRm", "LicenseManager", "LxpSvc", "MSDTC", "MSiSCSI", "McpManagementService",
-        "MicrosoftEdgeElevationService", "MsKeyboardFilter", "NaturalAuthentication", "NcaSvc",
-        "NcbService", "NcdAutoSetup", "NetSetupSvc", "Netman", "NgcCtnrSvc", "NgcSvc", "NlaSvc",
-        "PcaSvc", "PeerDistSvc", "PerfHost", "PhoneSvc", "PlugPlay", "PolicyAgent", "PrintNotify",
-        "PushToInstall", "QWAVE", "RasAuto", "RasMan", "RemoteAccess", "RemoteRegistry", "RetailDemo",
-        "RmSvc", "RpcLocator", "SCPolicySvc", "SCardSvr", "SDRSVC", "SEMgrSvc", "SNMPTRAP", "SSDPSRV",
-        "ScDeviceEnum", "SecurityHealthService", "Sense", "SensorDataService", "SensorService",
-        "SensrSvc", "SessionEnv", "SharedAccess", "SmsRouter", "SstpSvc", "StateRepository", "StiSvc",
-        "StorSvc", "TapiSrv", "TextInputManagementService", "TieringEngineService", "TimeBrokerSvc",
-        "TokenBroker", "TroubleshootingSvc", "TrustedInstaller", "UmRdpService", "UsoSvc", "VSS",
-        "W32Time", "WEPHOSTSVC", "WFDSConMgrSvc", "WManSvc", "WPDBusEnum", "WaaSMedicSvc",
-        "WalletService", "WarpJITSvc", "WbioSrvc", "WdiServiceHost", "WdiSystemHost", "WebClient",
-        "Wecsvc", "WerSvc", "WiaRpc", "WinHttpAutoProxySvc", "WinRM", "WpcMonSvc", "WpnService",
-        "XblAuthManager", "XblGameSave", "XboxGipSvc", "XboxNetApiSvc", "autotimesvc", "bthserv",
-        "camsvc", "cloudidsvc", "dcsvc", "defragsvc", "diagsvc", "dmwappushservice", "dot3svc",
-        "embeddedmode", "fdPHost", "fhsvc", "hidserv", "icssvc", "lfsvc", "lltdsvc", "lmhosts",
-        "msiserver", "netprofm", "perceptionsimulation", "pla", "seclogon", "smphost", "svsvc",
-        "swprv", "upnphost", "vds", "vmicguestinterface", "vmicheartbeat", "vmickvpexchange",
-        "vmicrdv", "vmicshutdown", "vmictimesync", "vmicvmsession", "vmicvss", "wbengine", "wcncsvc",
-        "webthreatdefsvc", "wuauserv", "wercplsupport", "wisvc", "wlidsvc", "wlpasvc", "wmiApSrv"
-    )
-    "Disabled" = @(
-        "AppVClient", "DiagTrack", "DialogBlockingService", "RemoteAccess",
-        "RemoteRegistry", "UevAgentService", "shpamsvc", "tzautoupdate"
-    )
-    "DelayedAuto" = @(
-        "BITS", "wscsvc", "sppsvc", "WSearch", "DoSvc"
-    )
-}
-
-foreach ($startupType in $serviceGroups.Keys) {
-    foreach ($service in $serviceGroups[$startupType]) {
-        try {
-            if ($startupType -eq "DelayedAuto") {
-                sc.exe config $service start= delayed-auto *> $null
-            } else {
-                Set-Service -Name $service -StartupType $startupType -ErrorAction Stop
-            }
-        } catch {}
-    }
-}
-
-$wildcards = @(
-    "BcastDVRUserService_*", "BluetoothUserService_*", "CDPUserSvc_*", "CaptureService_*",
-    "ConsentUxUserSvc_*", "CredentialEnrollmentManagerUserSvc_*", "DeviceAssociationBrokerSvc_*",
-    "DevicePickerUserSvc_*", "DevicesFlowUserSvc_*", "MessagingService_*", "NPSMSvc_*",
-    "P9RdrService_*", "PenService_*", "PimIndexMaintenanceSvc_*", "PrintWorkflowUserSvc_*",
-    "UdkUserSvc_*", "UnistoreSvc_*", "UserDataSvc_*", "WpnUserService_*", "cbdhsvc_*",
-    "webthreatdefusersvc_*"
-)
-
-foreach ($pattern in $wildcards) {
-    Get-Service -Name $pattern -ErrorAction SilentlyContinue | ForEach-Object {
-        try {
-            if ($_.ServiceType -notlike "*UserService*") {
-                Set-Service -InputObject $_ -StartupType Manual
-            }
-        } catch {}
-    }
 }
 
 reg import $env:TEMP\file.reg
