6e0c47ca0031c5a42d52ff72be531a9dc45b284c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-25 18:14:15 +0300

    Update SystemTweaks.ps1

--- SystemTweaks.ps1
+++ SystemTweaks.ps1
@@ -1,7 +1,7 @@
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value ((Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1kb) -Force; Set-ExecutionPolicy Bypass -Scope LocalMachine -Force
 Remove-Item "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl" -ErrorAction SilentlyContinue; icacls "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger" /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
 iwr "https://raw.githubusercontent.com/GabiNun/Scripts/main/file.reg" -o "$env:TEMP\file.reg"; iwr "https://raw.githubusercontent.com/GabiNun/Scripts/main/file.ps1" -o "$env:TEMP\file.ps1"; reg import $env:TEMP\file.reg; & "$env:TEMP\file.ps1"
-iwr "https://aka.ms/vs/17/release/vc_redist.x64.exe" -o "$env:TEMP\v.exe"; sp.exe "$env:TEMP\v.exe" "/install /quiet" -w
+iwr https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile $env:TEMP\v.exe; Start-Process $env:TEMP\v.exe -Args '/install','/quiet' -Wait
 [Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
