794e2c52fe04fe1c8ef9a573f757b2b61f7160d9
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-25 13:53:23 +0300

    Update SystemTweaks.ps1

--- SystemTweaks.ps1
+++ SystemTweaks.ps1
@@ -1,6 +1,6 @@
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value ((Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1kb) -Force
 Remove-Item "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl" -ErrorAction SilentlyContinue; icacls "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger" /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
-Curl https://raw.githubusercontent.com/GabiNun/Scripts/main/file.reg -OutFile $env:TEMP\file.reg; Curl https://raw.githubusercontent.com/GabiNun/Scripts/main/file.ps1 -OutFile $env:TEMP\file.ps1
+iwr "https://raw.githubusercontent.com/GabiNun/Scripts/main/file.reg" -o "$env:TEMP\file.reg"; iwr "https://raw.githubusercontent.com/GabiNun/Scripts/main/file.ps1" -o "$env:TEMP\file.ps1"
 iwr "https://aka.ms/vs/17/release/vc_redist.x64.exe" -o "$env:TEMP\v.exe"; sp "$env:TEMP\v.exe" "/install /quiet" -w
 [Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
 powershell -ExecutionPolicy Bypass -File "$env:TEMP\file.ps1"
