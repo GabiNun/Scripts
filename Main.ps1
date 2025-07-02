4f80aa9f584c8bfd22bcfc3e16e71b905c190d7f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-03 02:00:15 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -38,7 +38,6 @@ Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desk
 $ProgressPreference = 'SilentlyContinue'
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 Get-AppxPackage | ? {!$_.NonRemovable} | Remove-AppxPackage *> $null
-iwr raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.reg -OutFile $env:TEMP\R.reg; regedit /s $env:TEMP\R.reg
 
 $store='HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'; $appx=Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI"
 $extraSids = if (Test-Path $store) { Get-ChildItem $store -ea 0 | ForEach-Object { $_.PSChildName } | Where-Object { $_ -like 'S-1-5-21*' } } else { @() }
