0eaf1ced390fa2f813784e4ff0956ae5008cbefc
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-03 01:27:21 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -43,3 +43,4 @@ $store='HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'; 
 $extraSids = if (Test-Path $store) { Get-ChildItem $store -ea 0 | ForEach-Object { $_.PSChildName } | Where-Object { $_ -like 'S-1-5-21*' } } else { @() }
 foreach ($sid in @('S-1-5-18') + $extraSids) { New-Item "$store\EndOfLife\$sid\$($appx.PackageFullName)" -Force | Out-Null }; New-Item "$store\Deprovisioned\$($appx.PackageFamilyName)" -Force | Out-Null
 DISM /Online /Set-NonRemovableAppPolicy /PackageFamily:$($appx.PackageFamilyName) /NonRemovable:0 | Out-Null; Remove-AppxPackage -AllUsers -Package $appx.PackageFullName *> $null
+iwr raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.reg -OutFile $env:TEMP\R.reg; regedit /s $env:TEMP\R.reg
