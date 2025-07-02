273b7fe1edf4d4f420685f32bd8f2e391723dbe4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-03 02:57:22 +0300

    Update RemoveEdge.ps1

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = 'SilentlyContinue'
+Stop-Process -Name msedge,MicrosoftEdgeUpdate -Force
 Remove-Item -LiteralPath "$env:ProgramFiles\WindowsApps\Microsoft.Edge.GameAssist*" -Force -Recurse
 Remove-Item -LiteralPath "$env:ProgramFiles\WindowsApps\Microsoft.MicrosoftEdge*" -Force -Recurse
 Remove-Item -LiteralPath "$env:ProgramFiles\WindowsApps\MicrosoftWindows.Client.WebExperience_424.1301.270.9_x64__cw5n1h2txyewy\Dashboard\StaticWidgetRegistrations\images\microsoftedge*" -Force -Recurse
