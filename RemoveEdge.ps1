5edfe4a1f2be91fd560284d54200a809b499a5cb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-03 03:58:26 +0300

    Update RemoveEdge.ps1

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,3 +1,5 @@
 Get-Process | Where-Object { $_.Name -like '*edge*' } | Stop-Process -Force
 Get-ChildItem C:\ -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.Name -like '*edge*' } | ForEach-Object { try { takeown /F "$($_.FullName)" /A | Out-Null; cmd /c "icacls `"$($_.FullName)`" /grant `"$env:USERNAME`":F" | Out-Null; Remove-Item "$($_.FullName)" -Force -Recurse | Out-Null } catch { } }
-takeown /F "C:\Program Files\Internet Explorer" | Out-Null; icacls "C:\Program Files\Internet Explorer" /grant "$($env:USERNAME):(F)" | Out-Null; Remove-Item -Path "C:\Program Files\Internet Explorer" -Recurse -Force | Out-Null
+takeown /F "C:\Program Files\Internet Explorer" | Out-Null
+icacls "C:\Program Files\Internet Explorer" /grant "$($env:USERNAME):(F)" | Out-Null
+Remove-Item -Path "C:\Program Files\Internet Explorer" -Recurse -Force | Out-Null
