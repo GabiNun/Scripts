44b6595b341bda44de70b65a188b95e6d440f0fc
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-03 12:37:53 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -56,3 +56,7 @@ foreach ($item in $matches) {
 takeown /F "C:\Program Files\Internet Explorer" /R /D Y | Out-Null
 icacls "C:\Program Files\Internet Explorer" /grant "$($env:USERNAME):(F)" /T | Out-Null
 Remove-Item -Path "C:\Program Files\Internet Explorer" -Recurse -Force
+
+takeown /F "C:\Program Files (x86)\Internet Explorer" /R /D Y | Out-Null
+icacls "C:\Program Files (x86)\Internet Explorer" /grant "$($env:USERNAME):(F)" /T | Out-Null
+Remove-Item -Path "C:\Program Files (x86)\Internet Explorer" -Recurse -Force
