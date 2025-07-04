f98ae5215c099279a19adef1ad2d3c0b56da73b3
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-04 17:33:20 +0300

    Create RemoveDefender.ps1

---
+++ RemoveDefender.ps1
@@ -0,0 +1,102 @@
+irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/RemoveSecHealthApp.ps1 | iex
+
+$tempFolder = "$env:TEMP"
+
+$urls = @(
+    "https://github.com/ionuttbara/windows-defender-remover/raw/main/PowerRun.exe",
+    "https://github.com/ionuttbara/windows-defender-remover/raw/main/PowerRun.ini",
+    "https://github.com/ionuttbara/windows-defender-remover/raw/main/Remove_Defender/RemoveDefender.reg",
+    "https://github.com/ionuttbara/windows-defender-remover/raw/main/Remove_SecurityComp/Remove_SecurityComp.reg"
+)
+
+foreach ($url in $urls) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $destination = Join-Path -Path $tempFolder -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $destination
+}
+
+$PowerRunExe = Join-Path -Path $tempFolder -ChildPath "PowerRun.exe"
+$RemoveDefenderReg = Join-Path -Path $tempFolder -ChildPath "RemoveDefender.reg"
+$RemoveSecurityCompReg = Join-Path -Path $tempFolder -ChildPath "Remove_SecurityComp.reg"
+
+Start-Process -FilePath $PowerRunExe -ArgumentList "reg import `"$RemoveDefenderReg`"" -Wait
+Start-Process -FilePath $PowerRunExe -ArgumentList "reg import `"$RemoveSecurityCompReg`"" -Wait
+
+$deleteFiles = @(
+    "C:\Windows\WinSxS\FileMaps\wow64_windows-defender*.manifest",
+    "C:\Windows\WinSxS\FileMaps\x86_windows-defender*.manifest",
+    "C:\Windows\WinSxS\FileMaps\amd64_windows-defender*.manifest",
+    "C:\Windows\System32\SecurityAndMaintenance_Error.png",
+    "C:\Windows\System32\SecurityAndMaintenance.png",
+    "C:\Windows\System32\SecurityHealthSystray.exe",
+    "C:\Windows\System32\SecurityHealthService.exe",
+    "C:\Windows\System32\SecurityHealthHost.exe",
+    "C:\Windows\System32\drivers\SgrmAgent.sys",
+    "C:\Windows\System32\drivers\WdDevFlt.sys",
+    "C:\Windows\System32\drivers\WdBoot.sys",
+    "C:\Windows\System32\drivers\WdFilter.sys",
+    "C:\Windows\System32\wscsvc.dll",
+    "C:\Windows\System32\drivers\WdNisDrv.sys",
+    "C:\Windows\System32\wscproxystub.dll",
+    "C:\Windows\System32\wscisvif.dll",
+    "C:\Windows\System32\SecurityHealthProxyStub.dll",
+    "C:\Windows\System32\smartscreen.dll",
+    "C:\Windows\SysWOW64\smartscreen.dll",
+    "C:\Windows\System32\smartscreen.exe",
+    "C:\Windows\SysWOW64\smartscreen.exe",
+    "C:\Windows\System32\DWWIN.EXE",
+    "C:\Windows\SysWOW64\smartscreenps.dll",
+    "C:\Windows\System32\smartscreenps.dll",
+    "C:\Windows\System32\SecurityHealthCore.dll",
+    "C:\Windows\System32\SecurityHealthSsoUdk.dll",
+    "C:\Windows\System32\SecurityHealthUdk.dll",
+    "C:\Windows\System32\SecurityHealthAgent.dll",
+    "C:\Windows\System32\wscapi.dll",
+    "C:\Windows\System32\wscadminui.exe",
+    "C:\Windows\SysWOW64\GameBarPresenceWriter.exe",
+    "C:\Windows\System32\GameBarPresenceWriter.exe",
+    "C:\Windows\SysWOW64\DeviceCensus.exe",
+    "C:\Windows\SysWOW64\CompatTelRunner.exe",
+    "C:\Windows\system32\drivers\msseccore.sys",
+    "C:\Windows\system32\drivers\MsSecFltWfp.sys",
+    "C:\Windows\system32\drivers\MsSecFlt.sys"
+)
+
+foreach ($file in $deleteFiles) {
+    Start-Process -FilePath $PowerRunExe -ArgumentList "cmd.exe /c del /f `"$file`"" -Wait
+}
+
+$removeDirs = @(
+    "C:\Windows\WinSxS\amd64_security-octagon*",
+    "C:\Windows\WinSxS\x86_windows-defender*",
+    "C:\Windows\WinSxS\wow64_windows-defender*",
+    "C:\Windows\WinSxS\amd64_windows-defender*",
+    "C:\Windows\SystemApps\Microsoft.Windows.AppRep.ChxApp_cw5n1h2txyewy",
+    "C:\ProgramData\Microsoft\Windows Defender",
+    "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection",
+    "C:\Program Files (x86)\Windows Defender Advanced Threat Protection",
+    "C:\Program Files\Windows Defender Advanced Threat Protection",
+    "C:\ProgramData\Microsoft\Windows Security Health",
+    "C:\ProgramData\Microsoft\Storage Health",
+    "C:\WINDOWS\System32\drivers\wd",
+    "C:\Program Files (x86)\Windows Defender",
+    "C:\Program Files\Windows Defender",
+    "C:\Windows\System32\SecurityHealth",
+    "C:\Windows\System32\WebThreatDefSvc",
+    "C:\Windows\System32\Sgrm",
+    "C:\Windows\Containers\WindowsDefenderApplicationGuard.wim",
+    "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\DefenderPerformance",
+    "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DefenderPerformance",
+    "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Defender",
+    "C:\Windows\System32\Tasks_Migrated\Microsoft\Windows\Windows Defender",
+    "C:\Windows\System32\Tasks\Microsoft\Windows\Windows Defender",
+    "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\Defender",
+    "C:\Windows\System32\HealthAttestationClient",
+    "C:\Windows\GameBarPresenceWriter",
+    "C:\Windows\bcastdvr",
+    "C:\Windows\Containers\serviced\WindowsDefenderApplicationGuard.wim"
+)
+
+foreach ($dir in $removeDirs) {
+    Start-Process -FilePath $PowerRunExe -ArgumentList "cmd.exe /c rmdir /s /q `"$dir`"" -Wait
+}
