6c7921f5f99a7eee963ebaa9950de73d0b564cf5
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-13 14:47:48 -0700

    Update Defender.ps1

--- Defender.ps1
+++ Defender.ps1
@@ -1,37 +1,34 @@
-gci C:\ -r -fo -ea 0 | ? Name -match 'Defender|Security' | ri -r -fo -ea 0
-
 $file = @(
-  "C:\Windows\System32\drivers\SgrmAgent.sys",
-  "C:\Windows\System32\drivers\WdDevFlt.sys",
-  "C:\Windows\System32\drivers\WdBoot.sys",
-  "C:\Windows\System32\drivers\WdFilter.sys",
-  "C:\Windows\System32\drivers\WdNisDrv.sys",
-  "C:\Windows\system32\drivers\msseccore.sys",
-  "C:\Windows\system32\drivers\MsSecFltWfp.sys",
-  "C:\Windows\system32\drivers\MsSecFlt.sys",
-  "C:\Windows\System32\wscproxystub.dll",
-  "C:\Windows\System32\wscsvc.dll",
-  "C:\Windows\System32\wscisvif.dll",
+  "C:\Windows\WinSxS\FileMaps\*windows-defender*.manifest",
+  "C:\Windows\System32\*SecurityHealth*.exe",
+  "C:\Windows\System32\*SecurityHealth*.dll",
+  "C:\Windows\System32\SecurityAndMaintenance*.png",
+  "C:\Windows\System32\wsc*.dll",
   "C:\Windows\System32\smartscreen*",
   "C:\Windows\SysWOW64\smartscreen*",
   "C:\Windows\System32\DWWIN.EXE",
-  "C:\Windows\System32\wscapi.dll",
-  "C:\Windows\System32\wscadminui.exe",
-  "C:\Windows\SysWOW64\GameBarPresenceWriter.exe",
   "C:\Windows\System32\GameBarPresenceWriter.exe",
+  "C:\Windows\SysWOW64\GameBarPresenceWriter.exe",
   "C:\Windows\SysWOW64\DeviceCensus.exe",
   "C:\Windows\SysWOW64\CompatTelRunner.exe"
 )
 
 $dirs = @(
-  "C:\Windows\SystemApps\Microsoft.Windows.AppRep.ChxApp_cw5n1h2txyewy",
-  "C:\ProgramData\Microsoft\Storage Health",
-  "C:\WINDOWS\System32\drivers\wd",
-  "C:\Windows\System32\WebThreatDefSvc",
+  "C:\ProgramData\Microsoft\Windows Defender*",
+  "C:\Program Files*\Windows Defender*",
+  "C:\Windows\System32\Tasks*\Microsoft\Windows\Windows Defender",
+  "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Defender*",
+  "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\Defender*",
+  "C:\Windows\SystemApps\Microsoft.Windows.AppRep*",
+  "C:\Windows\System32\SecurityHealth",
   "C:\Windows\System32\Sgrm",
+  "C:\Windows\System32\WebThreatDefSvc",
   "C:\Windows\System32\HealthAttestationClient",
   "C:\Windows\GameBarPresenceWriter",
-  "C:\Windows\bcastdvr"
+  "C:\Windows\bcastdvr",
+  "C:\Windows\Containers\*WindowsDefenderApplicationGuard.wim",
+  "C:\Windows\WinSxS\*windows-defender*",
+  "C:\Windows\WinSxS\*security-octagon*"
 )
 
 $file | ? { Test-Path $_ } | % { ri $_ -Force }
