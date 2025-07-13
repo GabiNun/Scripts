0508d7c9d9bc5ea0b76ab75ff7d4f542fdf397ba
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-13 08:00:39 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -43,8 +43,5 @@ $ProgressPreference = 'SilentlyContinue'
 Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
 ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 | ? Name -match 'edge' | ri -r -fo -ea 0
 
-$a = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-WindowStyle Hidden -Command irm raw.githubusercontent.com/GabiNun/Scripts/main/Defender.ps1 | iex"
-Register-ScheduledTask -TaskName 'Defender' -Action $a;$svc = New-Object -ComObject 'Schedule.Service';$svc.Connect();$user = 'NT SERVICE\TrustedInstaller'
-$folder = $svc.GetFolder('\');$task = $folder.GetTask('Defender');$task.RunEx($null, 0, 0, $user)
-
+Register-ScheduledTask -TaskName 'Defender' -Action (New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command `"irm 'https://raw.githubusercontent.com/GabiNun/Scripts/main/Defender.ps1' | iex`"") -Force; $svc=New-Object -ComObject 'Schedule.Service'; $svc.Connect(); $svc.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'; $appx = Get-AppxPackage -Name Microsoft.SecHealthUI; $sids = @('S-1-5-18'); $sids += Get-ChildItem $store -ea 0 | % { $_.PSChildName } | ? { $_.StartsWith('S-1-5-21') }; New-Item -Path "$store\Deprovisioned\$($appx.PackageFamilyName)" -ItemType RegistryKey -Force | Out-Null; foreach ($sid in $sids) { New-Item -Path "$store\EndOfLife\$sid\$($appx.PackageFullName)" -ItemType RegistryKey -Force | Out-Null }; $appx | Remove-AppxPackage
