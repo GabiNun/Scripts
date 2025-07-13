789faa05376eec68a0df1d1275bf6f4a30d76dbf
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-13 11:23:23 -0700

    Update Defender.ps1

--- Defender.ps1
+++ Defender.ps1
@@ -1,38 +1,9 @@
 $file = @(
-  "C:\Windows\WinSxS\FileMaps\*windows-defender*.manifest",
-  "C:\Windows\System32\*SecurityHealth*.exe",
-  "C:\Windows\System32\*SecurityHealth*.dll",
-  "C:\Windows\System32\SecurityAndMaintenance*.png",
-  "C:\Windows\System32\wsc*.dll",
-  "C:\Windows\System32\smartscreen*",
-  "C:\Windows\SysWOW64\smartscreen*",
-  "C:\Windows\System32\DWWIN.EXE",
-  "C:\Windows\System32\drivers\Wd*",
-  "C:\Windows\System32\drivers\SgrmAgent.sys",
-  "C:\Windows\System32\drivers\MsSecFlt*",
-  "C:\Windows\System32\drivers\msseccore.sys",
-  "C:\Windows\System32\GameBarPresenceWriter.exe",
-  "C:\Windows\SysWOW64\GameBarPresenceWriter.exe",
-  "C:\Windows\SysWOW64\DeviceCensus.exe",
-  "C:\Windows\SysWOW64\CompatTelRunner.exe"
+  "C:\Windows\System32\smartscreen*"
 )
 
 $dirs = @(
-  "C:\ProgramData\Microsoft\Windows Defender*",
-  "C:\Program Files*\Windows Defender*",
-  "C:\Windows\System32\Tasks*\Microsoft\Windows\Windows Defender",
-  "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Defender*",
-  "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\Defender*",
-  "C:\Windows\SystemApps\Microsoft.Windows.AppRep*",
-  "C:\Windows\System32\SecurityHealth",
-  "C:\Windows\System32\Sgrm",
-  "C:\Windows\System32\WebThreatDefSvc",
-  "C:\Windows\System32\HealthAttestationClient",
-  "C:\Windows\GameBarPresenceWriter",
-  "C:\Windows\bcastdvr",
-  "C:\Windows\Containers\*WindowsDefenderApplicationGuard.wim",
-  "C:\Windows\WinSxS\*windows-defender*",
-  "C:\Windows\WinSxS\*security-octagon*"
+  "C:\Program Files*\Windows Defender*"
 )
 
 $file | ? { Test-Path $_ } | % { ri $_ -Force }
