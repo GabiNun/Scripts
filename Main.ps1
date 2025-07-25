ef94a6ede6bc8556c652b1b6fb6955bcdd85cd64
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 10:09:49 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,6 +1,12 @@
-$ProgressPreference = 'SilentlyContinue'
-irm raw.githubusercontent.com/GabiNun/FireWall/main/Apply.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/RegEdit.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveApps.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.ps1.ps1 | iex
-powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60
+$cmds = @(
+  "irm raw.githubusercontent.com/GabiNun/FireWall/main/Apply.ps1 | iex",
+  "irm raw.githubusercontent.com/GabiNun/Scripts/main/RegEdit.ps1 | iex",
+  "irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveApps.ps1 | iex",
+  "irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.ps1.ps1 | iex",
+  "powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c",
+  "powercfg /change monitor-timeout-ac 60"
+)
+
+foreach ($c in $cmds) {
+  Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -WindowStyle Hidden -Command `$ProgressPreference='SilentlyContinue'; $c"
+}
