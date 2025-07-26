d2737b61abca32eae569d6723b2e6dc83aa5669e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-26 11:28:27 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -2,5 +2,5 @@ $ProgressPreference = 'SilentlyContinue'
 irm raw.githubusercontent.com/GabiNun/FireWall/main/Apply.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Scripts/main/RegEdit.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveApps.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.ps1.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveDefender.ps1 | iex
 powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60
