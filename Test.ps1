4e768a7e4aa8d8211b8d922dee1a92928e0705ad
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-02 16:49:37 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,3 @@
-foreach ($svc in (irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json).WPFTweaksServices.service | Where-Object { $_.Name -and $_.StartupType }) {
+foreach ($svc in (irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json).WPFTweaksServices.service | Where-Object { $_.Name -and $_.StartupType }) {
     Set-Service -Name $svc.Name -StartupType $svc.StartupType
 }
