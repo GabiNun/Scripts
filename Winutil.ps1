0fb928f0abdf4403ec576d2dced04f5064e98f12
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-02 18:16:19 -0700

    Update Winutil.ps1

--- Winutil.ps1
+++ Winutil.ps1
@@ -1,3 +1,4 @@
-irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json | ForEach-Object { $_.WPFTweaksServices.service } | Where-Object { $_.Name -and $_.StartupType } | ForEach-Object { Set-Service -Name $_.Name -StartupType $_.StartupType -ea 0 }
-irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json | ForEach-Object { $_.WPFTweaksTele.registry } | ForEach-Object { Set-ItemProperty -Path $_.Path -Name $_.Name -Value $_.Value -Type $_.Type -ea 0 }
-irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json | ForEach-Object { $_.WPFTweaksTele.ScheduledTask } | ForEach-Object { Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null }
+($json=irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json) | Out-Null
+$json.WPFTweaksServices.service | Where-Object { $_.Name -and $_.StartupType } | ForEach-Object { Set-Service -Name $_.Name -StartupType $_.StartupType -ea 0 }
+$json.WPFTweaksTele.registry | ForEach-Object { Set-ItemProperty -Path $_.Path -Name $_.Name -Value $_.Value -Type $_.Type -ea 0 }
+$json.WPFTweaksTele.ScheduledTask | ForEach-Object { Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null }
