f1267236ca7cee340fe41ed6c7b941906641eeac
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-02 18:05:33 -0700

    Update Winutil.ps1

--- Winutil.ps1
+++ Winutil.ps1
@@ -2,18 +2,7 @@ foreach ($svc in (irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/conf
     Set-Service -Name $svc.Name -StartupType $svc.StartupType -ea 0
 }
 
-Invoke-WebRequest raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json -UseBasicParsing |
-Select-Object -ExpandProperty Content | ConvertFrom-Json |
-% { $_.WPFTweaksTele.registry | % {
-    if (-not (Test-Path $_.Path)) { New-Item -Path $_.Path -Force | Out-Null }
-    if ($_.Value -ne "<RemoveEntry>") {
-        $v = if ($_.Type -eq "DWord") { [uint32]$_.Value } else { $_.Value }
-        if (Get-ItemProperty -Path $_.Path -Name $_.Name -ea 0) {
-            Set-ItemProperty -Path $_.Path -Name $_.Name -Value $v | Out-Null
-        } else {
-            New-ItemProperty -Path $_.Path -Name $_.Name -Value $v -PropertyType $_.Type -ea 0 | Out-Null
-        }
-    } else {
-        Remove-ItemProperty -Path $_.Path -Name $_.Name -ea 0 | Out-Null
-    }
-} }
+irm "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json"|
+Select-Object -ExpandProperty WPFTweaksTele|
+Select-Object -ExpandProperty ScheduledTask|
+ForEach-Object {Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null}
