5ff169761936aae3c4c205f08a0e3976cead6c80
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-02 18:09:48 -0700

    Update Winutil.ps1

--- Winutil.ps1
+++ Winutil.ps1
@@ -1,4 +1,2 @@
-irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json|ForEach-Object { $_.WPFTweaksTele.ScheduledTask } | ForEach-Object { Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null }
-foreach ($svc in (irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json).WPFTweaksServices.service | Where-Object { $_.Name -and $_.StartupType }) {
-    Set-Service -Name $svc.Name -StartupType $svc.StartupType -ea 0
-}
+irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json | ForEach-Object { $_.WPFTweaksServices.service } | Where-Object { $_.Name -and $_.StartupType } | ForEach-Object { Set-Service -Name $_.Name -StartupType $_.StartupType -ea 0 }
+irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json | ForEach-Object { $_.WPFTweaksTele.ScheduledTask } | ForEach-Object { Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null }
