75e56a7a9a9d1299ac74634cbe142973901efc99
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-02 17:50:48 -0700

    Update Winutil.ps1

--- Winutil.ps1
+++ Winutil.ps1
@@ -6,9 +6,25 @@ Invoke-WebRequest raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/t
 Select-Object -ExpandProperty Content | ConvertFrom-Json |
 % { $_.WPFTweaksConsumerFeatures.registry | % {
     if (-not (Test-Path $_.Path)) { New-Item $_.Path -Force | Out-Null }
-    if (Get-ItemProperty -Path $_.Path -Name $_.Name -ErrorAction SilentlyContinue) {
+    if (Get-ItemProperty -Path $_.Path -Name $_.Name -ea 0) {
         Set-ItemProperty $_.Path $_.Name ([int]$_.Value) | Out-Null
     } else {
         New-ItemProperty $_.Path $_.Name ([int]$_.Value) -PropertyType $_.Type | Out-Null
+    }
+} }
+
+Invoke-WebRequest raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json -UseBasicParsing |
+Select-Object -ExpandProperty Content | ConvertFrom-Json |
+% { $_.WPFTweaksTele.registry | % {
+    if (-not (Test-Path $_.Path)) { New-Item -Path $_.Path -Force | Out-Null }
+    if ($_.Value -ne "<RemoveEntry>") {
+        $v = if ($_.Type -eq "DWord") { [uint32]$_.Value } else { $_.Value }
+        if (Get-ItemProperty -Path $_.Path -Name $_.Name -ea 0) {
+            Set-ItemProperty -Path $_.Path -Name $_.Name -Value $v | Out-Null
+        } else {
+            New-ItemProperty -Path $_.Path -Name $_.Name -Value $v -PropertyType $_.Type -ea 0 | Out-Null
+        }
+    } else {
+        Remove-ItemProperty -Path $_.Path -Name $_.Name -ea 0 | Out-Null
     }
 } }
