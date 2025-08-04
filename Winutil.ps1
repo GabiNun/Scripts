02c0e4d113f119d3323cfe06a3d3848a24e6fddb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-04 00:39:07 -0700

    Delete Winutil.ps1

--- Winutil.ps1
+++
@@ -1,4 +0,0 @@
-($json=irm raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/tweaks.json) | Out-Null
-$json.WPFTweaksServices.service | Where-Object { $_.Name -and $_.StartupType } | ForEach-Object { Set-Service -Name $_.Name -StartupType $_.StartupType -ea 0 }
-$json.WPFTweaksTele.registry | ForEach-Object { Set-ItemProperty -Path $_.Path -Name $_.Name -Value $_.Value -Type $_.Type -ea 0 }
-$json.WPFTweaksTele.ScheduledTask | ForEach-Object { Disable-ScheduledTask -TaskName $_.Name -ea 0 | Out-Null }
