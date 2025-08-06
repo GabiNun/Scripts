83253ef7ffa8d2e9251555c938d68f34bbf97609
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 21:18:44 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,5 @@
 $ProgressPreference = 'SilentlyContinue'
 irm raw.githubusercontent.com/GabiNun/Scripts/main/RegEdit.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveApps.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Disable-Windows-Defender/main/Main.ps1 | iex
 powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60
