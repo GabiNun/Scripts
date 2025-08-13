912d23be794f285aec741e10a18d9d509f37ef55
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-12 20:17:27 -0700

    Delete RemoveApps.ps1

--- RemoveApps.ps1
+++
@@ -1,3 +0,0 @@
-& $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
-Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
-ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 | ? Name -match 'edge' | ri -r -fo -ea 0
