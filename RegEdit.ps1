8045ff4b5da71ab27a0717cf8f5ae6fe2b23970a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-25 19:18:23 +0300

    Update RegEdit.ps1

--- RegEdit.ps1
+++ RegEdit.ps1
@@ -5,6 +5,7 @@ ni HKCU:\Software\Policies\Microsoft\Windows\Explorer | Out-Null
 
 Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer HideRecommendedSection 1
 Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableNotificationCenter 1
+Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableSearchBoxSuggestions 1
 Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start HideRecommendedSection 1
 Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education IsEducationEnvironment 1
 Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel '{645FF040-5081-101B-9F08-00AA002F954E}' 1
