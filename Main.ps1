e7845f650b16e9ff9cb492c84e23ed2758fc416b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-25 19:30:33 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -4,6 +4,6 @@ irm raw.githubusercontent.com/GabiNun/Disable-Windows-Defender/main/Main.ps1 | i
 powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60
 
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
-Get-AppxPackage|?{!$_.NonRemovable}|rm -ea 0
-Get-AppxPackage|?{!-_.NonRemovable}|rm -ea 0
+Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
+Get-AppxPackage|?{!-_.NonRemovable}|Remove-AppxPackage -ea 0
 ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 | ? Name -match 'edge' | ri -r -fo -ea 0
