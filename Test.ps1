1fcd4b6150502baed2599bfb866f1887dafcdb1d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-25 07:42:31 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -5,3 +5,8 @@ powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monito
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
 ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 | ? Name -match 'edge' | ri -r -fo -ea 0
+
+Rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk" -Fo
+Rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk" -Fo
+Rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility" -Recurse -Fo
+Rm 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge' -Fo
