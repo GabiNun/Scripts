0a1dbb390578a86d7d40d6661666d63e6c7041e4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-25 07:41:45 -0700

    Create Test.ps1

---
+++ Test.ps1
@@ -0,0 +1,7 @@
+$ProgressPreference = 'SilentlyContinue'
+irm raw.githubusercontent.com/GabiNun/Disable-Windows-Defender/main/Main.ps1 | iex
+powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60
+
+& $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
+Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
+ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 | ? Name -match 'edge' | ri -r -fo -ea 0
