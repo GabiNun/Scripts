32778ed536430225ea823d53ec09c2c44213c492
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-04 18:14:18 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -6,4 +6,4 @@ powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monito
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
 ps *edge*|spps -fo; gci C:\ -r -fo -ea 0 -filter *edge* | ri -r -fo -ea 0
-#spps msedge -f; gci C:\ -r -fo -ea 0 -filter *edge* | ri -r -fo -ea 0
+#kill msedge; gci C:\ -r -fo -ea 0 -filter *edge* | ri -r -fo -ea 0
