4667f8e0b87373bdee860482d6b286b9fca1d9e0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-06 00:01:17 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,7 +1,7 @@
-"HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education",
-"HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start",
-"HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer",
-"HKCU:\Software\Policies\Microsoft\Windows\Explorer" | % { ni $_ | Out-Null }
+ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education|Out-Null
+ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start|Out-Null
+ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer|Out-Null
+ni HKCU:\Software\Policies\Microsoft\Windows\Explorer|Out-Null
 
 sp HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR AppCaptureEnabled 0
 sp HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer HideRecommendedSection 1
@@ -9,10 +9,9 @@ sp HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableNotificationCenter 
 sp HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableSearchBoxSuggestions 1
 sp HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start HideRecommendedSection 1
 sp HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education IsEducationEnvironment 1
-sp HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel '{645FF040-5081-101B-9F08-00AA002F954E}' 1
+sp HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel {645FF040-5081-101B-9F08-00AA002F954E} 1
 
-"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk",
-"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility",
-"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk" | % { if(Test-Path $_){rm $_ -Recurse -Force} }
-
-rm 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge'
+rm $env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk
+rm $env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility -R -Fo
+rm $env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk
+rm HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge
