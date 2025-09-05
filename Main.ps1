2930c18aa45b64fe69bcafc04bcd492e3747dbd0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-05 23:49:33 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,7 +1,7 @@
 $ProgressPreference = 'SilentlyContinue'
 irm raw.githubusercontent.com/GabiNun/Scripts/main/Settings.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Disable-Windows-Defender/main/Main.ps1 | iex
-powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c;powercfg /change monitor-timeout-ac 60;powercfg /h off
+powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
 
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
