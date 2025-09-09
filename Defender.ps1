5d32f1f0f54df9b4059a7ac72ca2f31c28a048f6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-09 14:11:40 +0300

    Update Defender.ps1

--- Defender.ps1
+++ Defender.ps1
@@ -1,6 +1,2 @@
-irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_Defender/RemoveDefender.reg -o $Env:Temp\Defender.reg # From https://github.com/ionuttbara/windows-defender-remover
-Register-ScheduledTask Defender -Action (New-ScheduledTaskAction reg.exe "import $env:TEMP\Defender.reg")|Out-Null
-$o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
-
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage *SecHealthUI
 $sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' });$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null };$appx | Remove-AppxPackage
