f28ac400cc92a1ba9cf426b8e440835899c8a20b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-13 11:35:12 +0300

    Create Apps.ps1

---
+++ Apps.ps1
@@ -0,0 +1,3 @@
+& $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
+Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
+spps -N *edge* -f;gci C:\ -r -fo -ea 0 -filter *edge*|ri -r -fo -ea 0
