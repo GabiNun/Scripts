d619ea5082e1b6c78594b8c256aa14e8c8bb5808
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-13 11:34:40 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -6,8 +6,3 @@ powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
 spps -N *edge* -f;gci C:\ -r -fo -ea 0 -filter *edge*|ri -r -fo -ea 0
-Get-AppxPackage | ? NonRemovable -eq $false | Remove-AppxPackage -EA 0
-
-
-Measure-Command { Get-AppxPackage | ? NonRemovable -eq $false | Remove-AppxPackage -EA 0 }
-Measure-Command { Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0 }
