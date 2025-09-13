d77c0655ebd00aabba81d26d621833a849efebe3
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-13 11:35:29 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,8 +1,5 @@
 $ProgressPreference = 'SilentlyContinue'
 irm raw.githubusercontent.com/GabiNun/Scripts/main/Settings.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Scripts/main/Defender.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Scripts/main/Apps.ps1 | iex
 powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
-
-& $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
-Get-AppxPackage|?{!$_.NonRemovable}|Remove-AppxPackage -ea 0
-spps -N *edge* -f;gci C:\ -r -fo -ea 0 -filter *edge*|ri -r -fo -ea 0
