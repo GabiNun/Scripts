02dda32851afe71dcb83888f158af370c5a31c95
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-13 11:35:55 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,5 +1,5 @@
 $ProgressPreference = 'SilentlyContinue'
 irm raw.githubusercontent.com/GabiNun/Scripts/main/Settings.ps1 | iex
 irm raw.githubusercontent.com/GabiNun/Scripts/main/Defender.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/Apps.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Scripts/main/RemoveApps.ps1 | iex
 powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
