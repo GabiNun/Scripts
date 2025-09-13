f66a87cac27a70958c542a40f0d719f3a84ee371
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-13 11:38:57 +0300

    Update Settings.ps1

--- Source/Settings.ps1
+++ Source/Settings.ps1
@@ -15,5 +15,3 @@ rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
 rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility" -R -Fo
 rm "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
 rm 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge'
-
-powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
