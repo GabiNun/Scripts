55df9da7740361f70582ea2b15348d14b18683a6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 13:18:35 +0300

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,6 +1,7 @@
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
-spps -N -F msedge,MicrosoftEdgeUpdate
+spps -N msedge
+spps -N -F MicrosoftEdgeUpdate
 rm -R -Fo "C:\Program Files (x86)\Microsoft"
 rm "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
 rm "C:\Users\Public\Desktop\Microsoft Edge.lnk"
