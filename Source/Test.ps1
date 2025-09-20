e2ec8e098b2da2b465f648d63543a8ec459b0c4f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 16:20:58 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -11,5 +11,3 @@ if ($policy) {
     $policy.defaultState = "enabled"
     $policyData | ConvertTo-Json -Depth 100 | Set-Content $jsonPath -Encoding UTF8
 }
-
-#     "C:\Program Files (x86)\Microsoft\Edge\Application\140.0.3485.81\Installer\setup.exe" --uninstall --msedge --channel=stable --system-level --verbose-logging
