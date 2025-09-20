3480a80184494324ee17401ff88e791c9d43516b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 16:33:18 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,5 +0,0 @@
-$jsonPath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json";$guidValue = "{1bca278a-5d11-4acf-ad2f-f9ab6d7f93a6}"
-
-takeown /f $jsonPath;icacls $jsonPath /grant Administrators:F
-
-(gc $jsonPath -Raw | ConvertFrom-Json | % {($_.Policies | ? guid -eq $guidValue).defaultState="enabled"; $_} | ConvertTo-Json -Depth 100) | sc $jsonPath -Encoding UTF8
