2f4c9eee78042f9846ed0ba61cfb09e1220733d0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 16:10:33 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -4,6 +4,8 @@ $guidValue = "{1bca278a-5d11-4acf-ad2f-f9ab6d7f93a6}"
 takeown /f $jsonPath
 icacls $jsonPath /grant Administrators:F
 
+
+$policyData = Get-Content $jsonPath -Raw | ConvertFrom-Json
 $policy = $policyData.Policies | Where-Object guid -eq $guidValue
 
 $policyData | ConvertTo-Json -Depth 100 | Set-Content $jsonPath -Encoding UTF8
