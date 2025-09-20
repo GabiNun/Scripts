777ec14bbd301bb4964929f2adc91d015a0f76da
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 15:56:47 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,18 +1,13 @@
-takeown /f "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
-icacls "C:\Windows\System32\IntegratedServicesRegionPolicySet.json" /grant Administrators:F
-
-$filePath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
-
-$text = Get-Content -Raw -Path $filePath
-
+$jsonPath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
 $guid = "{1bca278a-5d11-4acf-ad2f-f9ab6d7f93a6}"
 
-$pattern = '(?<={[^}]*"guid"\s*:\s*"' + [regex]::Escape($guid) + '"[^}]*"defaultState"\s*:\s*")[^"]+'
+takeown /f $jsonPath
+icacls $jsonPath /grant Administrators:F
 
-$replacement = "enabled"
+$policyData = Get-Content $jsonPath -Raw | ConvertFrom-Json
+$policy = $policyData.Policies | Where-Object Id -eq $guid
 
-$newText = [regex]::Replace($text, $pattern, $replacement)
-
-Set-Content -Path $filePath -Value $newText -Force -Encoding UTF8
-
-& "C:\Program Files (x86)\Microsoft\Edge\Application\140.0.3485.81\Installer\setup.exe" --uninstall --msedge --channel=stable --system-level --verbose-logging
+if ($policy) {
+    $policy.DefaultState = "Enabled"
+    $policyData | ConvertTo-Json -Depth 100 | Set-Content $jsonPath -Encoding UTF8
+}
