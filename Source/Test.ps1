580c5c89d1bc7382fbb8d0a6912cf0875d1de20b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 17:51:07 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,3 +0,0 @@
-Get-AppxPackage -AllUsers | Where-Object {
-    $_.Name -notmatch 'Microsoft.WindowsStore|Microsoft.Windows.Cortana|Microsoft.DesktopAppInstaller|Microsoft.Windows.StartMenuExperienceHost|Microsoft.Windows.ShellExperienceHost|Microsoft.Windows.Shell|Microsoft.Windows.People|Microsoft.Windows.OOBENetworkCaptivePortal|Microsoft.Windows.Search|Microsoft.Windows.CloudExperienceHost|Microsoft.Windows.SecureAssessmentBrowser'
-} | Remove-AppxPackage -ErrorAction 0
