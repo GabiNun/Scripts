227855a7f1eea33aff36ba323de28776d8ad224a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 16:31:10 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,7 +1,5 @@
-$jsonPath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
-$guidValue = "{1bca278a-5d11-4acf-ad2f-f9ab6d7f93a6}"
+$jsonPath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json";$guidValue = "{1bca278a-5d11-4acf-ad2f-f9ab6d7f93a6}"
 
-takeown /f $jsonPath
-icacls $jsonPath /grant Administrators:F
+takeown /f $jsonPath;icacls $jsonPath /grant Administrators:F
 
 (gc $jsonPath -Raw | ConvertFrom-Json | % {($_.Policies | ? guid -eq $guidValue).defaultState="enabled"; $_} | ConvertTo-Json -Depth 100) | sc $jsonPath -Encoding UTF8
