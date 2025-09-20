12bcd9c8fb87099814a41bed4f0da148ae7e568c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-20 17:47:01 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,3 +1,3 @@
 Get-AppxPackage -AllUsers | Where-Object {
-    $_.Name -notmatch 'Microsoft.WindowsStore|Microsoft.Windows.Cortana|Microsoft.DesktopAppInstaller|Microsoft.Windows.StartMenuExperienceHost|Microsoft.Windows.ShellExperienceHost|Microsoft.Windows.CBS' 
+    $_.Name -notmatch 'Microsoft.DesktopAppInstaller|Microsoft.Windows.StartMenuExperienceHost|Microsoft.Windows.ShellExperienceHost|Microsoft.Windows.CBS' 
 } | Remove-AppxPackage -ErrorAction 0
