408cf0e958f97ef3ece441183e69c1e65bdf0cdb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-22 13:32:36 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -5,5 +5,15 @@ if (-not (Test-Path $RegPath)) {
     New-Item -Path $RegPath\DisallowRun
 }
 
-New-ItemProperty -Path "$RegPath\DisallowRun" -Name 1 -Value "msedge.exe"
+New-ItemProperty -Path "$RegPath\DisallowRun" -Name DisableEdge -Value "msedge.exe"
 New-ItemProperty -Path "$RegPath" -Name "DisallowRun" -Value 1 -PropertyType DWord
+
+Get-ScheduledTask -TaskPath "\" |
+Where-Object {$_.TaskName -like "MicrosoftEdge*"} |
+ForEach-Object {
+    Disable-ScheduledTask -TaskName $_.TaskName -TaskPath "\"
+}
+
+## Undo Script
+
+Remove-ItemProperty -Path "$RegPath\DisallowRun" -Name DisableEdge
