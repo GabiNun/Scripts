5daae2401f014e499123e1ffa7aca531040149d7
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-22 13:07:41 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -3,5 +3,6 @@ New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explore
 New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" -Force
 
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" -Name 1 -Value "msedge.exe" -Force
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" -Name 1 -Value "MicrosoftEdgeUpdate.exe" -Force
 
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisallowRun" -Value 1 -PropertyType DWord -Force
