8d0a5bc223dd8848e2a5dc7d401452f341de6b5f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-22 13:23:09 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,9 +1,9 @@
 $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 
-New-Item -Path "$RegPath" -Force
-
-New-Item -Path "$RegPath\DisallowRun" -Force
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath
+    New-Item -Path $RegPath\DisallowRun
+}
 
 New-ItemProperty -Path "$RegPath\DisallowRun" -Name 1 -Value "msedge.exe" -Force
-
 New-ItemProperty -Path "$RegPath" -Name "DisallowRun" -Value 1 -PropertyType DWord -Force
