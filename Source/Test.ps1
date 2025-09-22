c8636062c206bb79e82374f27f5c2ec2c41e94a4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-22 13:11:16 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,7 +1,9 @@
-New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Force
+$RegPath = HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer
 
-New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" -Force
+New-Item -Path "$RegPath" -Force
 
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" -Name 1 -Value "msedge.exe" -Force
+New-Item -Path "$RegPath\DisallowRun" -Force
 
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisallowRun" -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path "$RegPath\DisallowRun" -Name 1 -Value "msedge.exe" -Force
+
+New-ItemProperty -Path "$RegPath" -Name "DisallowRun" -Value 1 -PropertyType DWord -Force
