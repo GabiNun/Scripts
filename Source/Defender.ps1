adf2e2fa0c643da78201c5171ca8966cf9478a39
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-24 14:12:20 -0700

    Create Defender.ps1

---
+++ Source/Defender.ps1
@@ -0,0 +1,5 @@
+Register-ScheduledTask Defender -Action (New-ScheduledTaskAction -Execute powershell -Argument "-Command rm -r -fo 'C:\Program Files*\Windows Defender*',C:\WINDOWS\system32\SecurityHealthService.exe,C:\Windows\System32\SecurityHealthSystray.exe")|Out-Null
+$o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
+
+$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage *SecHealthUI
+$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' });$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null };$appx | Remove-AppxPackage
