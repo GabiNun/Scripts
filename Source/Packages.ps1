500a4997c71877c709efd583e3df4d4f32b58156
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-24 13:52:40 -0700

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -5,6 +5,8 @@ rm -R -Fo "C:\Program Files (x86)\Microsoft"
 rm "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 rm "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
 
+$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage *SecHealthUI
+$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' });$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null };$appx | Remove-AppxPackage
 Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage -ea 0
 Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage -ea 0
 Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage -ea 0
