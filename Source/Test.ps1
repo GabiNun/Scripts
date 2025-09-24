584ca6dfd0dc34f8908c5a3f469995d1a7f0a715
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-24 11:19:40 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,3 +1,3 @@
-Register-ScheduledTask -TaskName Test -Action (New-ScheduledTaskAction -Execute powershell -Argument "-Command rm -r -fo 'C:\Program Files*\Windows Defender*',C:\WINDOWS\system32\SecurityHealthService.exe,C:\Windows\System32\SecurityHealthSystray.exe") -Principal (New-ScheduledTaskPrincipal -UserId "SYSTEM" )
+Register-ScheduledTask -TaskName "Test" -Action (New-ScheduledTaskAction -Execute cmd)
 
 Start-ScheduledTask -TaskName "Test"
