6cf2dd4dc8b6673d239deb66f6cc5117970851a4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-24 13:52:58 -0700

    Delete Source/Defender.ps1

--- Source/Defender.ps1
+++
@@ -1,2 +0,0 @@
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage *SecHealthUI
-$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' });$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null };$appx | Remove-AppxPackage
