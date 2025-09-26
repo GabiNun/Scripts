3ea2e4da5cfe2ce940d3f2be753384fc2446d5af
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-26 23:34:03 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" IsReadOnly $true
-Set-ItemProperty "C:\Program Files (x86)\Microsoft\EdgeUpdate\MicrosoftEdgeUpdate.exe" IsReadOnly $true
