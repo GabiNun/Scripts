93033b2f1d6b01f85d48b782ca00d3c1d08ea42d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-26 23:32:23 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,2 +1,2 @@
 Set-ItemProperty "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" IsReadOnly $true
-PS C:\Users\Gabi> Set-ItemProperty "C:\Program Files (x86)\Microsoft\EdgeUpdate\MicrosoftEdgeUpdate.exe" IsReadOnly $true
+Set-ItemProperty "C:\Program Files (x86)\Microsoft\EdgeUpdate\MicrosoftEdgeUpdate.exe" IsReadOnly $true
