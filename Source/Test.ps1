5bd54c03168e51335b68b6baded1e7ca8bd624d8
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-27 16:40:06 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,5 +1,5 @@
 $json = Invoke-RestMethod "https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/config/tweaks.json"
 
 foreach ($s in $json.WPFTweaksServices.service) {
-    sc.exe config $s.Name start= $s.StartupType | Out-Null
+    sc.exe config $s.Name start= $s.StartupType
 }
