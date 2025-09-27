4d2658bd8d8367121cba9a50c982beb230d93d51
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-27 20:06:22 +0300

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,8 +1,7 @@
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
 spps -n *edge* -f
-rm -R "C:\Program Files (x86)\Microsoft"
-rm "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+rm -R "C:\Program Files (x86)\Microsoft","C:\Users\Public\Desktop\Microsoft Edge.lnk"
 rm "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
 
 Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage
