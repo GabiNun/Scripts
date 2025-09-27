dc57c035fad0a78fd3942d3bb93d4f6247c491a0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-27 17:03:06 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,16 +1,12 @@
 $json = Invoke-RestMethod "https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/config/tweaks.json"
 
 foreach ($s in $json.WPFTweaksServices.service) {
-    sc.exe config $s.Name start= $s.StartupType
-}
-
-
-foreach ($s in $json.WPFTweaksServices.service) {
     $startType = switch ($s.StartupType.ToLower()) {
         "automatic" { "auto" }
         "manual"    { "demand" }
         "disabled"  { "disabled" }
+        "AutomaticDelayedStart" { "delayed-auto" }
     }
 
-    sc.exe config $s.Name start= $startType | Out-Null
+    sc.exe config $s.Name start= $startType
 }
