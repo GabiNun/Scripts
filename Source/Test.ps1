895b9d685d3ea0bc64ec994338249f8a71d31e88
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-29 17:29:35 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -2,6 +2,6 @@ $uninstall = (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\Cur
 $edgeUWP   = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 
 New-Item $edgeUWP -ItemType Directory
-New-Item "$edgeUWP\MicrosoftEdge.exe"
+New-Item $edgeUWP\MicrosoftEdge.exe
 
 Start-Process cmd "/c $uninstall" -WindowStyle Hidden
