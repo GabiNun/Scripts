f09cd7d0042a29d53d22c38f7800da0ec0241b6b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-29 17:24:59 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,10 +1,6 @@
 $uninstall = (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge").UninstallString + " --force-uninstall"
 $edgeUWP   = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 
-New-Item $edgeUWP -ItemType Directory
 New-Item "$edgeUWP\MicrosoftEdge.exe"
 
 Start-Process cmd "/c $uninstall" -WindowStyle Hidden -Wait
-
-Remove-Item "$edgeUWP\MicrosoftEdge.exe"
-Remove-Item $edgeUWP
