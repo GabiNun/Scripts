7b4a8c08b6458d132c160e9d8a0941d2a2f11a54
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-29 17:13:53 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,27 +1,21 @@
-$regView = [Microsoft.Win32.RegistryView]::Registry32
-$reg = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $regView).
-OpenSubKey('SOFTWARE\Microsoft', $true)
+$reg = [Microsoft.Win32.RegistryKey]::OpenBaseKey(
+    [Microsoft.Win32.RegistryHive]::LocalMachine,
+    [Microsoft.Win32.RegistryView]::Registry32
+).OpenSubKey('SOFTWARE\Microsoft', $true)
 
-$uninstall = $reg.OpenSubKey('Windows\CurrentVersion\Uninstall\Microsoft Edge').
-GetValue('UninstallString') + ' --force-uninstall'
+$uninstall = $reg.OpenSubKey('Windows\CurrentVersion\Uninstall\Microsoft Edge').GetValue('UninstallString') + ' --force-uninstall'
+$fakeHost  = "$env:SystemRoot\SystemTemp\dllhost.exe"
+$edgeUWP   = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 
-$fakeDllhost = "$env:SystemRoot\SystemTemp\dllhost.exe"
-
-Copy-Item "$env:SystemRoot\System32\cmd.exe" $fakeDllhost
-
-$reg.OpenSubKey('EdgeUpdate\ClientState\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}', $true).
-DeleteValue('experiment_control_labels')
-
+Copy-Item "$env:SystemRoot\System32\cmd.exe" $fakeHost
+$reg.OpenSubKey('EdgeUpdate\ClientState\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}', $true).DeleteValue('experiment_control_labels')
 $reg.CreateSubKey('EdgeUpdateDev').SetValue('AllowUninstall', '')
 
-$edgeUWP = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 New-Item $edgeUWP -ItemType Directory
 New-Item "$edgeUWP\MicrosoftEdge.exe"
 
-Start-Process $fakeDllhost "/c $uninstall" -WindowStyle Hidden -Wait
+Start-Process $fakeHost "/c $uninstall" -WindowStyle Hidden -Wait
 
 Remove-Item "$edgeUWP\MicrosoftEdge.exe"
-Remove-Item $fakeDllhost
+Remove-Item $fakeHost
 Remove-Item $edgeUWP
-
-Write-Output "Edge should now be uninstalled!"
