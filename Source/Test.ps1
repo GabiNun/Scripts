7bdcf2de5897f09e1cae2d0794930c5d13911f41
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-29 18:10:41 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,4 +0,0 @@
-New-Item $env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe -I D
-New-Item $env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe
-
-cmd /c ((Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge').UninstallString + ' --force-uninstall')
