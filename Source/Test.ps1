59af1880edaea6dab6f176c40c1bde2ce7691c06
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-04 09:05:34 -0400

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,5 +1,5 @@
 Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "-C rm -r -fo 'C:\Program Files*\Windows Defender*',C:\WINDOWS\system32\SecurityHealthService.exe,C:\Windows\System32\SecurityHealthSystray.exe") | Out-Null
 
-$svc = New-Object -ComObject 'Schedule.Service'
+$svc = New-Object -Com 'Schedule.Service'
 $svc.Connect()
 $svc.GetFolder('\').GetTask('Defender').RunEx("", 0, 0, 'NT SERVICE\TrustedInstaller')
