edd9af728ffb6e305aefb638fa7003ced526427e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-04 09:37:08 -0400

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,4 @@
 $ProgressPreference = 'SilentlyContinue'
-Measure-Command { irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Settings.ps1 | iex }
-Measure-Command { irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Defender.ps1 | iex }
-Measure-Command { irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Packages.ps1 | iex }
+irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Settings.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Defender.ps1 | iex
+irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Packages.ps1 | iex
