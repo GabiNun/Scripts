07f546cb8c4797587ca9c12abe1507482a1489f6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-08 18:40:39 +0000

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,4 @@
 $ProgressPreference = 'SilentlyContinue'
-irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Settings.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Defender.ps1 | iex
-irm raw.githubusercontent.com/GabiNun/Scripts/main/Source/Packages.ps1 | iex
+irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex
+irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex
+irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex
