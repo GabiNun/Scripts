86db20ea735e165e1a5794689c7a52fffd142afb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-11 19:11:09 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,3 +0,0 @@
-ni $env:SystemRoot\SystemApps\Microsoft.MicrosoftEdgeCore_8wekyb3d8bbwe\MicrosoftEdgeCore.exe -f
-
-& "C:\Program Files (x86)\Microsoft\EdgeCore\141.0.3537.71\Installer\setup.exe" --uninstall --system-level --force-uninstall
