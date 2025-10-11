9834db95ef8a88f14865ce551f33dd675ef6eabc
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-11 19:08:20 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,3 +1,3 @@
-ni $env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdgeCore.exe -f
+ni $env:SystemRoot\SystemApps\Microsoft.MicrosoftEdgeCore_8wekyb3d8bbwe\MicrosoftEdgeCore.exe -f
 
 & "C:\Program Files (x86)\Microsoft\EdgeCore\141.0.3537.71\Installer\setup.exe" --uninstall --system-level --force-uninstall
