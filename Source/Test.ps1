fe649960db7b9f373abb1673dd7697c824a8a328
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-12 22:43:11 +0000

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,8 +1,8 @@
 sp HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR AppCaptureEnabled 0
-ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer -N HideRecommendedSection -V 1
-ni HKCU:\Software\Policies\Microsoft\Windows\Explorer -N DisableNotificationCenter -V 1
-ni HKCU:\Software\Policies\Microsoft\Windows\Explorer -N DisableSearchBoxSuggestions -V 1
-ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start -N HideRecommendedSection -V 1
-ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education -N IsEducationEnvironment -V 1
-ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -N ExcludeWUDriversInQualityUpdate -V 1
-ni HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel -N '{645FF040-5081-101B-9F08-00AA002F954E}' -V 1
+ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer -N HideRecommendedSection -V 1 -f
+ni HKCU:\Software\Policies\Microsoft\Windows\Explorer -N DisableNotificationCenter -V 1 -f
+ni HKCU:\Software\Policies\Microsoft\Windows\Explorer -N DisableSearchBoxSuggestions -V 1 -f
+ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start -N HideRecommendedSection -V 1 -f
+ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education -N IsEducationEnvironment -V 1 -f
+ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -N ExcludeWUDriversInQualityUpdate -V 1 -f
+ni HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel -N '{645FF040-5081-101B-9F08-00AA002F954E}' -V 1 -f
