a53e66335e870420317bd0ff6dd5fc60954fa66d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-13 10:02:39 +0000

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,2 +0,0 @@
-Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell '-c Get-AppxPackage *SecHealthUI | Remove-AppxPackage') -User 'NT SERVICE\TrustedInstaller'
-Start-ScheduledTask Defender
