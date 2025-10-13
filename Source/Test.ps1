f51cd7cd3b909575687cf710456af46b5b0b6d76
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-13 10:24:38 +0000

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,2 +1,2 @@
-New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\-1-5-21-3939049671-854807330-977275038-1001\Microsoft.SecHealthUI_cw5n1h2txyewy" -Force
+New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\-1-5-21-3939049671-854807330-977275038-1001\Microsoft.SecHealthUI" -Force
 Get-AppxPackage *SecHealthUI | Remove-AppxPackage
