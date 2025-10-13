3abc9cb499efb931734b95bf3ea22ca3a62b60a2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-13 12:29:46 +0000

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,5 +1,6 @@
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
+Get-AppxPackage Microsoft.WindowsNotepad | Remove-AppxPackage
 Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage
 Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
 Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
