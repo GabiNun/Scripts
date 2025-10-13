8af769c239c464ac7a4f60331b2e2e699c33f249
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-13 10:26:19 +0000

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,2 +0,0 @@
-New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\-1-5-21-3939049671-854807330-977275038-1001\Microsoft.SecHealthUI" -Force
-Get-AppxPackage *SecHealthUI | Remove-AppxPackage
