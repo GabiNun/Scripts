c6c1b3e5bca404de939da5b6a22353966b29cfdf
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-16 21:21:10 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,4 +0,0 @@
-irm gist.github.com/GabiNun/53d72fc294b5da4c1c3199b0fbf264f3/raw/Defender.reg -OutFile $Env:Temp\Defender.reg
-
-Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell 'regedit.exe /s $Env:Temp\Defender.reg') -U 'NT SERVICE\TrustedInstaller'
-Start-ScheduledTask Defender
