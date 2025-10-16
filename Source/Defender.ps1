e3ad3bb6db6111f7b7ee295c0a25e2607fe226a2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-16 20:41:13 +0300

    Update Defender.ps1

--- Source/Defender.ps1
+++ Source/Defender.ps1
@@ -1,5 +1,5 @@
 Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "rm -r -fo 'C:\Program Files*\Windows Defender*',HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run") -U 'NT SERVICE\TrustedInstaller'
 Start-ScheduledTask Defender
 
-$sid = (glu $env:USERNAME).SID.Value;$appx = (Get-AppxPackage *SecHealthUI).PackageFullName
-ni HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$sid\$appx -f; Remove-AppxPackage $appx
+$Sid = (glu $env:USERNAME).Sid.Value;$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName
+ni HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -f; Remove-AppxPackage $appx
