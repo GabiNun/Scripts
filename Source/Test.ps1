81f929537be6efe2c6efb522e016d3bae77f2c8c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-16 21:08:37 +0300

    Create Test.ps1

---
+++ Source/Test.ps1
@@ -0,0 +1,4 @@
+irm gist.github.com/GabiNun/53d72fc294b5da4c1c3199b0fbf264f3/raw/Defender.reg -OutFile $Env:Temp\Defender.reg
+
+Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell 'reg import $Env:Temp\Defender.reg') -U 'NT SERVICE\TrustedInstaller'
+Start-ScheduledTask Defender
