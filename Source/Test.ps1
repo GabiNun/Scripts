50e2b35e2fea96e39d47e29d657c33156599a7c0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-18 12:10:54 +0300

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,4 +0,0 @@
-Stop-Process -Force -Name msedge,SearchHost,msedgewebview2,MicrosoftEdgeUpdate
-Remove-Item -Recurse -Force "C:\Program Files (x86)\Microsoft"
-Remove-Item "C:\Users\Public\Desktop\Microsoft Edge.lnk"
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
