ca8ed6e1fd33bffc8e8c04a693c1f2ef2d892bd2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-18 16:35:44 +0300

    Update Setup.ps1

--- Source/Setup.ps1
+++ Source/Setup.ps1
@@ -6,8 +6,7 @@ winget install fastfetch
 
 New-Item -Force $PROFILE -Value 'clear
 flashfetch
-$Host.UI.RawUI.ForegroundColor = "Blue"
 
 function prompt {
-    "[" + (Get-Location) + "] # "
+    "[" + (Get-Location) + "] # -ForegroundColor Blue"
 }'
