ab4623c50485835dc472e8fc8b5de983440923d9
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-18 11:18:30 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,2 +1,3 @@
 Stop-Process -Force -Name msedge,SearchHost,msedgewebview2,MicrosoftEdgeUpdate
 Remove-Item -Recurse -Force "C:\Program Files (x86)\Microsoft"
+Remove-Item "C:\Users\Public\Desktop\Microsoft Edge.lnk"
