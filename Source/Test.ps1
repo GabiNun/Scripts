6470dc2eb6e8da13e360f1b59c636039c9777dd5
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-18 11:16:18 +0300

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,2 +1,2 @@
-Stop-Process -Name msedge,SearchHost,msedgewebview2,MicrosoftEdgeUpdate -Force -ea 0
+Stop-Process -Force -Name msedge,SearchHost,msedgewebview2,MicrosoftEdgeUpdate
 Remove-Item -Recurse -Force "C:\Program Files (x86)\Microsoft"
