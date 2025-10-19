d419d4bad0590e795d0bd013fdeec009e3eed465
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-19 13:47:46 -0700

    Update 2Test.ps1

--- Source/2Test.ps1
+++ Source/2Test.ps1
@@ -1,1 +1,16 @@
-Remove-Item HKLM:\SOFTWARE\Classes\microsoft-edge -Recurse -Force
+$protocols = @(
+    "http", "https", "ftp", "mailto", "news", "nntp",
+    "snews", "telnet", "wais", "file", "ms-help",
+    "ms-settings", "microsoft-edge"
+)
+
+$fileAssociations = @(
+    ".htm", ".html", ".pdf", ".shtml", ".svg",
+    ".webp", ".xht", ".xhtml", ".mht", ".mhtml"
+)
+
+foreach ($protocol in $protocols) {
+    if ((Get-ItemProperty "HKLM:\SOFTWARE\Classes\$protocol\shell\open\command" 2>$null).'(default)' -like "*msedge*") {
+        Remove-Item "HKLM:\SOFTWARE\Classes\$protocol\shell\open" -Recurse -Force
+    }
+}
