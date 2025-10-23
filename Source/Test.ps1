a6319eeb5f1e22513d0db5113d2a7472bf5ac1ca
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-23 03:34:32 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -16,7 +16,7 @@ $edgePaths = @(
     "$env:LOCALAPPDATA\Microsoft\Edge",
     "$env:PROGRAMDATA\Microsoft\EdgeUpdate",
     "$env:PUBLIC\Desktop\Microsoft Edge.lnk",
-    "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk",
+    "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
 )
 
 $edgeRegKeys = @(
@@ -26,12 +26,12 @@ $edgeRegKeys = @(
     "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate",
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe",
     "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge",
-    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update"
-    "HKLM:\SOFTWARE\Classes\MSEdgeMHT"
-    "HKLM:\SOFTWARE\Classes\MSEdgeHTM"
-    "HKLM:\SOFTWARE\Classes\MSEdgePDF"
-    "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{9459C573-B17A-45AE-9F64-1857B5D58CEE}"
-    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}"
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update",
+    "HKLM:\SOFTWARE\Classes\MSEdgeMHT",
+    "HKLM:\SOFTWARE\Classes\MSEdgeHTM",
+    "HKLM:\SOFTWARE\Classes\MSEdgePDF",
+    "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{9459C573-B17A-45AE-9F64-1857B5D58CEE}",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}",
     "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}"
 )
 
