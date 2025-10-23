881ca0126abb2c68dd706e8b0050e4a57282b029
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-23 03:38:37 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -10,34 +10,34 @@
 Get-Process *edge* | Stop-Process -Force
 
 $edgePaths = @(
-    "${env:ProgramFiles(x86)}\Microsoft\Edge",
-    "${env:ProgramFiles(x86)}\Microsoft\EdgeUpdate",
-    "${env:ProgramFiles(x86)}\Microsoft\EdgeCore",
-    "$env:LOCALAPPDATA\Microsoft\Edge",
-    "$env:PROGRAMDATA\Microsoft\EdgeUpdate",
-    "$env:PUBLIC\Desktop\Microsoft Edge.lnk",
+    "${env:ProgramFiles(x86)}\Microsoft\Edge"
+    "${env:ProgramFiles(x86)}\Microsoft\EdgeUpdate"
+    "${env:ProgramFiles(x86)}\Microsoft\EdgeCore"
+    "$env:LOCALAPPDATA\Microsoft\Edge"
+    "$env:PROGRAMDATA\Microsoft\EdgeUpdate"
+    "$env:PUBLIC\Desktop\Microsoft Edge.lnk"
     "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
 )
 
 $edgeRegKeys = @(
-    "HKCU:\Software\Microsoft\Edge",
-    "HKLM:\SOFTWARE\Microsoft\Edge",
-    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Edge",
-    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate",
-    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe",
-    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge",
-    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update",
-    "HKLM:\SOFTWARE\Classes\MSEdgeMHT",
-    "HKLM:\SOFTWARE\Classes\MSEdgeHTM",
-    "HKLM:\SOFTWARE\Classes\MSEdgePDF",
-    "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{9459C573-B17A-45AE-9F64-1857B5D58CEE}",
-    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}",
+    "HKCU:\Software\Microsoft\Edge"
+    "HKLM:\SOFTWARE\Microsoft\Edge"
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Edge"
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate"
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe"
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge"
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update"
+    "HKLM:\SOFTWARE\Classes\MSEdgeMHT"
+    "HKLM:\SOFTWARE\Classes\MSEdgeHTM"
+    "HKLM:\SOFTWARE\Classes\MSEdgePDF"
+    "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{9459C573-B17A-45AE-9F64-1857B5D58CEE}"
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}"
     "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\Browser Helper Objects\{1FD49718-1D00-4B19-AF5F-070AF6D5D54C}"
 )
 
 $services = @(
-    "edgeupdate",
-    "edgeupdatem",
+    "edgeupdate"
+    "edgeupdatem"
     "MicrosoftEdgeElevationService"
 )
 
