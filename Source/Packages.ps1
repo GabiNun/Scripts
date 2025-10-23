8358e6e7bda4e9f968194ba2b8173f2d272b9f5d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-23 07:37:48 -0700

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -2,37 +2,18 @@ Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
-Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
-Get-AppxPackage MicrosoftCorporationII.QuickAssist | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
-Get-AppxPackage Microsoft.PowerAutomateDesktop | Remove-AppxPackage
-Get-AppxPackage Microsoft.StartExperiencesApp | Remove-AppxPackage
-Get-AppxPackage Microsoft.WebMediaExtensions | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-Get-AppxPackage MicrosoftWindows.CrossDevice | Remove-AppxPackage
-Get-AppxPackage Microsoft.OutlookForWindows | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage
-Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsTerminal | Remove-AppxPackage
-Get-AppxPackage Microsoft.Windows.DevHome | Remove-AppxPackage
-Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsStore | Remove-AppxPackage
-Get-AppxPackage Microsoft.Application* | Remove-AppxPackage
-Get-AppxPackage Microsoft.ScreenSketch | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingSearch | Remove-AppxPackage
-Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
-Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
-Get-AppxPackage Microsoft.GamingApp | Remove-AppxPackage
-Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage
-Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
-Get-AppxPackage Microsoft.Copilot | Remove-AppxPackage
-Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
-Get-AppxPackage Microsoft.Todos | Remove-AppxPackage
-Get-AppxPackage Microsoft.Paint | Remove-AppxPackage
-Get-AppxPackage MSTeams | Remove-AppxPackage
+$Packages = @(
+    "Microsoft.MicrosoftSolitaireCollection", "MicrosoftCorporationII.QuickAssist", "Microsoft.WindowsSoundRecorder",
+    "Microsoft.MicrosoftStickyNotes", "Microsoft.PowerAutomateDesktop", "Microsoft.StartExperiencesApp",
+    "Microsoft.WebMediaExtensions", "Microsoft.WindowsFeedbackHub", "Microsoft.MicrosoftOfficeHub",
+    "MicrosoftWindows.CrossDevice", "Microsoft.OutlookForWindows", "Microsoft.WindowsCalculator",
+    "Microsoft.XboxGamingOverlay", "Microsoft.WindowsTerminal", "Microsoft.Windows.DevHome", "Microsoft.Windows.Photos",
+    "Microsoft.WindowsCamera", "Microsoft.WindowsAlarms", "Microsoft.WindowsStore", "Microsoft.Application*",
+    "Microsoft.ScreenSketch", "Microsoft.BingWeather", "Microsoft.BingSearch", "Microsoft.YourPhone",
+    "Microsoft.Xbox.TCUI", "Microsoft.GamingApp", "Clipchamp.Clipchamp", "Microsoft.ZuneMusic", "Microsoft.BingNews",
+    "Microsoft.Copilot", "Microsoft.GetHelp", "Microsoft.Todos", "Microsoft.Paint", "MSTeams"
+)
+
+foreach ($Package in $Packages) {
+  Get-AppxPackage $Package | Remove-AppPackage
+}
