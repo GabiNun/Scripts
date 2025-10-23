375be40ac8bd07b4d299089e13cd481348f350c7
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-23 02:06:50 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -43,3 +43,14 @@ foreach ($service in $services) {
 }
 
 Stop-Process -Name explorer
+
+$protocols = @("http", "https", "ftp", "mailto", "news", "nntp", "snews", "telnet", "wais", "file", "ms-help", "ms-settings", "microsoft-edge")
+
+foreach ($protocol in $protocols) {
+    if ((Get-ItemProperty "HKLM:\SOFTWARE\Classes\$protocol\shell\open\command").'(default)' -like "*msedge*") {
+        Remove-Item "HKLM:\SOFTWARE\Classes\$protocol\shell\open" -Recurse -Force
+    }
+    if ((Get-ItemProperty "HKCU:\SOFTWARE\Classes\$protocol\shell\open\command").'(default)' -like "*msedge*") {
+        Remove-Item "HKCU:\SOFTWARE\Classes\$protocol\shell\open" -Recurse -Force
+    }
+}
