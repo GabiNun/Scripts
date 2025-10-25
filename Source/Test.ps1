ab905742ee565df425f4d58ea980b6df48a1051f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-25 08:47:14 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,7 +1,2 @@
-$regView = [Microsoft.Win32.RegistryView]::Registry32
-$microsoft = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $regView).
-OpenSubKey('SOFTWARE\Microsoft', $true)
-
-$uninstallRegKey = $microsoft.OpenSubKey('Windows\CurrentVersion\Uninstall\Microsoft Edge')
-$uninstallString = $uninstallRegKey.GetValue('UninstallString') + ' --force-uninstall'
+(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge').UninstallString + ' --force-uninstall'
 Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden
