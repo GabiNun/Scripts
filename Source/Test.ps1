3627edad8338d22fdc1497a7654c01d871186e13
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-25 08:48:42 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,2 +1,2 @@
-(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge').UninstallString + ' --force-uninstall'
+(Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge').UninstallString + ' --force-uninstall'
 Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden
