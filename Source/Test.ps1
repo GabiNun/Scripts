3ee6c665586a3ac2e3cdc53a71ad346f6bae2680
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-25 08:58:52 -0700

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,5 +1,3 @@
-Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\EdgeUpdate\ClientState\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}' -Name 'experiment_control_labels'
-
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\EdgeUpdateDev'
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\EdgeUpdateDev' -Name 'AllowUninstall' -Value '1'
 
