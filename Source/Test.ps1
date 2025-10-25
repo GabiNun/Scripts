6dcd1345fc6a89d372b448445a1268f4b1930ebf
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-25 09:01:48 -0700

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,4 +0,0 @@
-New-Item -Path 'HKLM:\SOFTWARE\Microsoft\EdgeUpdateDev'
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\EdgeUpdateDev' -Name 'AllowUninstall'
-
-(Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge').UninstallString + ' --force-uninstall'
