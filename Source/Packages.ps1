2623877c4fb852892d439e609c91bb2f4b0259e1
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-26 04:50:09 -0700

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -2,23 +2,6 @@ Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
 $Packages = @(
-    "Microsoft.Application*",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "MicrosoftCorporationII.QuickAssist",
-    "Microsoft.PowerAutomateDesktop",
-    "Microsoft.MicrosoftStickyNotes",
-    "Microsoft.Windows*",
-    "Microsoft.StartExperiencesApp",
-    "Microsoft.WebMediaExtensions",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.Xbox*",
-    "Microsoft.OutlookForWindows",
-    "Clipchamp.Clipchamp",
-    "Microsoft.Bing*",
-    "Microsoft.GetHelp",
-    "Microsoft.Todos",
-    "Microsoft.Paint",
-    "MSTeams"
 )
 
 foreach ($Package in $Packages) {Get-AppxPackage $Package | Remove-AppPackage}
