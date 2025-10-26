72b0a0bec75797518103f60fd8954f93addeaf6e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-26 00:25:52 -0700

    Update Settings.ps1

--- Source/Settings.ps1
+++ Source/Settings.ps1
@@ -1,17 +1,17 @@
-ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education
-ni HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start
-ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate
-ni HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer
-ni HKCU:\Software\Policies\Microsoft\Windows\Explorer
+New-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education
+New-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer
+New-Item HKCU:\Software\Policies\Microsoft\Windows\Explorer
 
-sp HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR AppCaptureEnabled 0
-sp HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer HideRecommendedSection 1
-sp HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableNotificationCenter 1
-sp HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableSearchBoxSuggestions 1
-sp HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start HideRecommendedSection 1
-sp HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education IsEducationEnvironment 1
-sp HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate ExcludeWUDriversInQualityUpdate 1
-sp HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel '{645FF040-5081-101B-9F08-00AA002F954E}' 1
+Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR AppCaptureEnabled 0
+Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer HideRecommendedSection 1
+Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableNotificationCenter 1
+Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableSearchBoxSuggestions 1
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start HideRecommendedSection 1
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education IsEducationEnvironment 1
+Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate ExcludeWUDriversInQualityUpdate 1
+Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel '{645FF040-5081-101B-9F08-00AA002F954E}' 1
 
 attrib +h "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility"
 attrib +h "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
