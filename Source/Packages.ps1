877d97a658d498a6efe3b4262c438a3704693c02
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-26 04:48:11 -0700

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,15 +1,24 @@
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
-$Packages = (
-    "Microsoft.MicrosoftSolitaireCollection", "MicrosoftCorporationII.QuickAssist", "Microsoft.WindowsSoundRecorder",
-    "Microsoft.MicrosoftStickyNotes", "Microsoft.PowerAutomateDesktop", "Microsoft.StartExperiencesApp",
-    "Microsoft.WebMediaExtensions", "Microsoft.WindowsFeedbackHub", "Microsoft.MicrosoftOfficeHub",
-    "MicrosoftWindows.CrossDevice", "Microsoft.OutlookForWindows", "Microsoft.WindowsCalculator",
-    "Microsoft.XboxGamingOverlay", "Microsoft.WindowsTerminal", "Microsoft.Windows.DevHome", "Microsoft.Windows.Photos",
-    "Microsoft.WindowsCamera", "Microsoft.WindowsAlarms", "Microsoft.WindowsStore", "Microsoft.Application*",
-    "Microsoft.ScreenSketch", "Microsoft.BingWeather", "Microsoft.BingSearch", "Microsoft.YourPhone",
-    "Microsoft.Xbox.TCUI", "Microsoft.GamingApp", "Clipchamp.Clipchamp", "Microsoft.ZuneMusic", "Microsoft.BingNews",
-    "Microsoft.Copilot", "Microsoft.GetHelp", "Microsoft.Todos", "Microsoft.Paint", "MSTeams"
+$Packages = @(
+    "Microsoft.Application*",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "MicrosoftCorporationII.QuickAssist",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.Windows*",
+    "Microsoft.StartExperiencesApp",
+    "Microsoft.WebMediaExtensions",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.Xbox*",
+    "Microsoft.OutlookForWindows",
+    "Clipchamp.Clipchamp",
+    "Microsoft.Bing*",
+    "Microsoft.GetHelp",
+    "Microsoft.Todos",
+    "Microsoft.Paint",
+    "MSTeams"
 )
+
 foreach ($Package in $Packages) {Get-AppxPackage $Package | Remove-AppPackage}
