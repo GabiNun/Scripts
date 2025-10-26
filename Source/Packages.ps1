2e777fe6cf5909f54752086709c91235a0df45e2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-26 04:50:39 -0700

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -2,6 +2,7 @@ Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
 $Packages = @(
+
 )
 
-foreach ($Package in $Packages) {Get-AppxPackage $Package | Remove-AppPackage}
+foreach ($Package in $Packages) { Get-AppxPackage $Package | Remove-AppPackage }
