34db3b1bda9f25d7ebf13560314641903649f3e7
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-01 15:34:18 +0200

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,6 +1,3 @@
-$chromeKey = "HKLM:\SOFTWARE\Policies\Google\Chrome"
-if (-not (Test-Path $chromeKey)) {
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Google" -Name "Chrome"
-}
+New-Item HKLM:\SOFTWARE\Policies\Google\Chrome
 
-New-ItemProperty -Path $chromeKey -Name "WebRTCIPHandlingPolicy" -Value 3 -PropertyType DWord -Force
+sp HKLM:\SOFTWARE\Policies\Google\Chrome WebRTCIPHandlingPolicy -Value 3
