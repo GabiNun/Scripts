7b2c74ea68cdea1ad76259899e52cc38190d70f0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-06 19:15:04 +0200

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,8 +1,7 @@
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
-$Packages = (
-    "Microsoft.WindowsCalculator","Microsoft.WindowsCamera","Microsoft.WindowsAlarms",
+$Packages = "Microsoft.WindowsCalculator","Microsoft.WindowsCamera","Microsoft.WindowsAlarms",
     "Microsoft.WindowsFeedbackHub","Microsoft.ZuneMusic","Microsoft.MicrosoftOfficeHub",
     "Microsoft.BingSearch","Microsoft.BingWeather","Microsoft.BingNews","Clipchamp.Clipchamp",
     "MSTeams","Microsoft.Todos","Microsoft.OutlookForWindows","Microsoft.Paint","Microsoft.YourPhone",
@@ -11,7 +10,6 @@ $Packages = (
     "Microsoft.StartExperiencesApp","Microsoft.MicrosoftStickyNotes","Microsoft.WindowsTerminal",
     "Microsoft.WebMediaExtensions","Microsoft.GamingApp","Microsoft.PowerAutomateDesktop",
     "Microsoft.Xbox.TCUI","Microsoft.Windows.DevHome","Microsoft.XboxGamingOverlay",
-    "Microsoft.GetHelp","Microsoft.WindowsStore","MicrosoftWindows.CrossDevice"
-)
+    "Microsoft.GetHelp","Microsoft.WindowsStore","MicrosoftWindows.CrossDevice","Microsoft.Edge.GameAssist"
 
 foreach ($Package in $Packages) { Get-AppxPackage $Package | Remove-AppPackage }
