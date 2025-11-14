cc808a625edbf6c08ebd158dbf1e00b3d1816921
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-14 17:22:43 +0200

    Update package removal filter for Notepad

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,3 +1,3 @@
-Get-AppxPackage | ? { !$_.IsFramework -and !$_.NonRemovable -and $_.Name -ne "Microsoft.WindowsNotepad" } | Remove-AppxPackage
+Get-AppxPackage | ? { !$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch Notepad } | Remove-AppxPackage
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $Env:SystemRoot\System32\OneDriveSetup.exe /uninstall
