38b5165652499350c3cc1d5e0d04e26f10540cf0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-14 19:40:10 +0200

    Fix formatting of Get-AppxPackage command

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,3 +1,4 @@
-Get-AppxPackage | ? { !$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|webexperience' } | Remove-AppxPackage
+Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|webexperience'} | Remove-AppxPackage
+
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $Env:SystemRoot\System32\OneDriveSetup.exe /uninstall
