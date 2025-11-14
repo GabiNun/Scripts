75d7c31322fe4f35176e037513be277e782b140a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-14 20:44:07 +0200

    Create Test.ps1

---
+++ Source/Test.ps1
@@ -0,0 +1,26 @@
+Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "rm -r -fo 'C:\Program Files*\Windows Defender*';sc.exe delete SecurityHealthService") -U 'NT SERVICE\TrustedInstaller'
+Start-ScheduledTask Defender
+
+New-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education
+New-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer
+New-Item HKCU:\Software\Policies\Microsoft\Windows\Explorer
+
+Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR AppCaptureEnabled 0
+Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableNotificationCenter 1
+Set-ItemProperty HKCU:\Software\Policies\Microsoft\Windows\Explorer DisableSearchBoxSuggestions 1
+
+Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer HideRecommendedSection 1
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start HideRecommendedSection 1
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Education IsEducationEnvironment 1
+
+attrib +h "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility"
+attrib +h "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
+attrib +h "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
+
+powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
+
+Stop-Process -Name MicrosoftEdgeUpdate -Force
+Remove-Item "$env:ProgramFiles (x86)\Microsoft\EdgeUpdate" -Recurse
+sc.exe delete edgeupdate  | Out-Null
+sc.exe delete edgeupdatem | Out-Null
