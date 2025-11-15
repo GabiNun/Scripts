c42914a042a67ff1f4bc63c0f27f85f60f3c7460
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-15 13:45:25 +0200

    Delete Source/Test.ps1

--- Source/Test.ps1
+++
@@ -1,7 +0,0 @@
-Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "rm -r -fo 'C:\Program Files*\Windows Defender*';sc.exe delete SecurityHealthService") -U 'NT SERVICE\TrustedInstaller' | Out-Null
-Start-ScheduledTask Defender | Out-Null
-
-Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection | Out-Null
-& $Env:SystemRoot\System32\OneDriveSetup.exe /uninstall
-
-irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex | Out-Null
