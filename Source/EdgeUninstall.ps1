8d7ed5598040d7463cc7512d9c2aa3b0cc9c1c0b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-21 22:02:21 +0200

    Create EdgeUninstall.ps1

---
+++ Source/EdgeUninstall.ps1
@@ -0,0 +1,24 @@
+$EdgePath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge"
+$EdgeUWP  = "$Env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
+
+New-Item $EdgeUWP\MicrosoftEdge.exe -Force | Out-Null
+
+if (Test-Path $EdgePath) {
+  cmd /c ((Get-ItemProperty $EdgePath).UninstallString + ' --force-uninstall --delete-profile')
+} else {
+    Write-Output "Edge is not installed"
+    return
+}
+
+Remove-Item $EdgeUWP -Recurse
+
+Get-Process *edge*,SearchHost | Stop-Process -Force
+Remove-Item "$env:ProgramFiles (x86)\Microsoft" -Recurse -Force
+sc.exe delete edgeupdate  | Out-Null
+sc.exe delete edgeupdatem | Out-Null
+Unregister-ScheduledTask *edge* -Confirm:$false
+
+$Appx = (Get-AppxPackage *EdgeDevToolsClient).PackageFullName
+$Sid = (glu $Env:USERNAME).Sid.Value
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null
+Remove-AppxPackage $Appx
