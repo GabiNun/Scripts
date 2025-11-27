b15b069e8b9bac42e13db657312c7042a8bd089d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-27 16:50:13 +0200

    Update test.ps1

--- Source/test.ps1
+++ Source/test.ps1
@@ -1,3 +1,3 @@
-atest = (irm https://launchermeta.mojang.com/mc/game/version_manifest.json).versions.url[0]
+Latest = (irm https://launchermeta.mojang.com/mc/game/version_manifest.json).versions.url[0]
 
 irm (irm $Latest).downloads.server.url -Out Server
