b781764310c5461c887e759e4ec2574ffa34f310
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-30 11:12:30 +0200

    Update Packages.ps1

--- Source/Packages.ps1
+++ Source/Packages.ps1
@@ -1,3 +1,4 @@
+Get-AppxPackage Microsoft.WindowsNotePad | Remove-AppxPackage
 Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'webexperience'} | Remove-AppxPackage
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
 & $Env:SystemRoot\System32\OneDriveSetup.exe /uninstall
