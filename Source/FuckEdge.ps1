0c66caf60a3661478925e1b700ae9949be9aa9f2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-08 21:55:58 +0200

    Update FuckEdge.ps1

--- Source/FuckEdge.ps1
+++ Source/FuckEdge.ps1
@@ -12,11 +12,11 @@ if (Test-Path $EdgePath) {
 
 Remove-Item $EdgeUWP -Recurse
 
-Get-Process *edge*,SearchHost | Stop-Process -Force
+Get-Process *Edge*,SearchHost | Stop-Process -Force
 Remove-Item "$Env:ProgramFiles (x86)\Microsoft" -Recurse -Force
 sc.exe delete edgeupdate  | Out-Null
 sc.exe delete edgeupdatem | Out-Null
-Unregister-ScheduledTask *edge* -Confirm:$false
+Unregister-ScheduledTask *Edge* -Confirm:$false
 
 $Appx = (Get-AppxPackage *EdgeDevToolsClient).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
 New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force;Remove-AppxPackage $Appx
