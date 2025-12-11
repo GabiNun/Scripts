0e46247bbd1f0d86ea41e0af70d684210f2c18e0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-11 18:22:26 +0200

    Delete Source/Install.ps1

--- Source/Install.ps1
+++
@@ -1,7 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-
-irm https://github.com/PowerShell/PowerShell/releases/download/v7.5.4/PowerShell-7.5.4-win-x64.zip -Out PowerShell-7.5.4-win-x64.zip
-
-Expand-Archive .\PowerShell-7.5.4-win-x64.zip 'C:\Program Files\PowerShell\7'
-
-Remove-Item .\PowerShell-7.5.4-win-x64.zip
