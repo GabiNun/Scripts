84167e1fadf3b44ba98523f0bd062b3884ca8bb3
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:36:47 +0200

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,6 +1,9 @@
 $Services =
-  "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService", "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV",
-  "WSAIFabricSvc", "BDESVC", "DispBrokerDesktopSvc", "DoSvc", "hidserv", "InventorySvc", "lmhosts", "NgcSvc", "NgcCtnrSvc", "RmSvc", "SharedAccess",
-  "smphost", "SysMain", "TextInputManagementService", "TimeBrokerSvc", "UdkUserSvc_*", "whesvc", "WpnService", "WpnUserService_*", "ClipSVC", "CDPUserSvc_*", "cbdhsvc_*", 
+    "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService",
+    "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc", "BDESVC",
+    "DispBrokerDesktopSvc", "DoSvc", "hidserv", "InventorySvc", "lmhosts", "NgcSvc",
+    "NgcCtnrSvc", "RmSvc", "SharedAccess", "smphost", "SysMain",
+    "TextInputManagementService", "TimeBrokerSvc", "UdkUserSvc_*", "whesvc",
+    "WpnService", "WpnUserService_*", "ClipSVC", "CDPUserSvc_*", "cbdhsvc_*"
 
 foreach ($Service in Get-Service $Services) { Set-Service $Service -StartupType Disabled }
