ca28d2d7086658162308e8743bd4e1c99fa37f8f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 14:02:07 +0200

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,10 +1,10 @@
 $ProgressPreference = 'SilentlyContinue'
 
 $Commands =
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex'
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex',
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex',
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex',
+  'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex',
   'irm github.com/GabiNun/Scripts/raw/main/Source/Services.ps1 | iex'
 
 foreach ($Command in $Commands) {
