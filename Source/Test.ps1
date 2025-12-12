ac5c6b7fc396dc688a72d21ad42176c0c738f5d0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:39:21 +0200

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,9 +1,7 @@
 $Services =
     "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService",
     "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc", "BDESVC",
-    "DispBrokerDesktopSvc", "DoSvc", "hidserv", "InventorySvc", "lmhosts", "NgcSvc",
-    "NgcCtnrSvc", "RmSvc", "SharedAccess", "smphost", "SysMain",
-    "TextInputManagementService", "TimeBrokerSvc", "UdkUserSvc_*", "whesvc",
-    "WpnService", "WpnUserService_*", "ClipSVC", "CDPUserSvc_*", "cbdhsvc_*"
+    "DispBrokerDesktopSvc", "hidserv", "InventorySvc", "lmhosts", "WpnService",
+    "RmSvc", "SharedAccess", "smphost", "SysMain", "whesvc"
 
 foreach ($Service in Get-Service $Services) { Set-Service $Service -StartupType Disabled }
