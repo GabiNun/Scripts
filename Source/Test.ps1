19439ec4182a95a3b1bf5074bc87805dd52766d4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:27:30 +0200

    Update Test.ps1

--- Source/Test.ps1
+++ Source/Test.ps1
@@ -1,3 +1,3 @@
-$Services = "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService", "NPSMSvc_*", "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc"
+$Services = "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService", "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc"
 
 foreach ($Service in Get-Service $Services) { Set-Service $Service -StartupType Disabled }
