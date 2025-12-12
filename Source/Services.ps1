bb2ab1680e38dcc86f3331fd969c6f4ebe16024a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 14:12:50 +0200

    Delete Source/Services.ps1

--- Source/Services.ps1
+++
@@ -1,7 +0,0 @@
-$Services =
-    "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService",
-    "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc", "BDESVC",
-    "DispBrokerDesktopSvc", "hidserv", "InventorySvc", "lmhosts", "WpnService", "whesvc",
-    "RmSvc", "smphost", "SysMain"
-
-foreach ($Service in Get-Service $Services) { if ($Service) { Set-Service $Service -StartupType Disabled } }
