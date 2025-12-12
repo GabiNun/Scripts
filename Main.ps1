96da40f8c7c19c6c274856b2fe0351b0de3763ed
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:43:16 +0200

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -3,3 +3,4 @@ irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex | Out-Null
+irm github.com/GabiNun/Scripts/raw/main/Source/Services.ps1 | iex
