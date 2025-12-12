4aae089159b8161f6c0d15ab645da2a0a8a1490c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 14:12:37 +0200

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -5,7 +5,6 @@ $Commands =
   'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex',
   'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex',
   'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex',
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Services.ps1 | iex'
 
 foreach ($Command in $Commands) {
     Start-Process powershell -WindowStyle Hidden -ArgumentList "-Command $Command"
