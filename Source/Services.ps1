69af5601232690245f453ab8c8c825f0840bc6a0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:47:09 +0200

    Update Services.ps1

--- Source/Services.ps1
+++ Source/Services.ps1
@@ -1,7 +1,7 @@
 $Services =
     "TrkWks", "camsvc", "DiagTrack", "DusmSvc", "InstallService", "lfsvc", "NcbService",
     "PcaSvc", "SENS", "SharedAccess", "Spooler", "SSDPSRV", "WSAIFabricSvc", "BDESVC",
-    "DispBrokerDesktopSvc", "hidserv", "InventorySvc", "lmhosts", "WpnService",
-    "RmSvc", "SharedAccess", "smphost", "SysMain", "whesvc"
+    "DispBrokerDesktopSvc", "hidserv", "InventorySvc", "lmhosts", "WpnService", "whesvc",
+    "RmSvc", "smphost", "SysMain"
 
 foreach ($Service in Get-Service $Services) { Set-Service $Service -StartupType Disabled }
