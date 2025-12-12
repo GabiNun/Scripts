dccb8f85c0b6dcfa13846445863d75a1ba12ec40
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-12 13:56:06 +0200

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,12 +1,12 @@
 $ProgressPreference = 'SilentlyContinue'
 
 $Commands =
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex | Out-Null'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex | Out-Null'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex | Out-Null'
-  'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex | Out-Null'
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex'
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex'
+  'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex'
+  'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex'
   'irm github.com/GabiNun/Scripts/raw/main/Source/Services.ps1 | iex'
 
 foreach ($Command in $Commands) {
-    Start-Process powershell -ArgumentList "-NoProfile -Command $Command"
+    Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command $Command"
 }
