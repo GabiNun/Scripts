4023fedf924407c02a216b9db74c3668a9a57eb5
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-13 22:40:48 +0200

    Update Defender.ps1

--- Source/Defender.ps1
+++ Source/Defender.ps1
@@ -4,4 +4,4 @@ Start-ScheduledTask Defender
 $Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
 New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force; Remove-AppxPackage $Appx
 
-ScheduledTask '*Defender*' | Unregister-ScheduledTask -Confirm:$False
+Get-ScheduledTask '*Defender*' | Unregister-ScheduledTask -Confirm:$False
