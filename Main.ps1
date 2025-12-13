20b8583e2a22f70301e068cdadecf0c311cebe73
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-13 22:53:20 +0200

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,11 +1,11 @@
 $ProgressPreference = 'SilentlyContinue'
 
-$Commands =
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1 | iex',
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1 | iex',
-  'irm github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1 | iex',
-  'irm github.com/GabiNun/Scripts/raw/main/Source/FuckEdge.ps1 | iex',
+$Urls =
+  'github.com/GabiNun/Scripts/raw/main/Source/Settings.ps1',
+  'github.com/GabiNun/Scripts/raw/main/Source/Defender.ps1',
+  'github.com/GabiNun/Scripts/raw/main/Source/Packages.ps1',
+  'gist.github.com/GabiNun/68d843555f96dbd21d4d47ef23eff5ce/raw/Edge.ps1'
 
-foreach ($Command in $Commands) {
-    Start-Process powershell -WindowStyle Hidden -ArgumentList "-Command $Command"
+foreach ($Url in $Urls) {
+    Start-Process powershell -WindowStyle Hidden -ArgumentList "-Command irm $Url | iex"
 }
