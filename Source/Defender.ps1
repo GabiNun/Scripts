23f4cadf8e53e824596ac74bd9d4ea410ff4d6db
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-13 19:52:46 +0200

    Update Defender.ps1

--- Source/Defender.ps1
+++ Source/Defender.ps1
@@ -1,4 +1,6 @@
-Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "rm -r -fo 'C:\Program Files*\Windows Defender*','C:\ProgramData\Microsoft\Windows Defender*';sc.exe delete 'C:\ProgramData\Microsoft\Windows Defender*'") -U 'NT SERVICE\TrustedInstaller'
+irm https://github.com/ionuttbara/windows-defender-remover/raw/refs/heads/main/Remove_Defender/RemoveDefender.reg -Out $Temp\RemoveDefender.reg
+
+Register-ScheduledTask Defender -Ac (New-ScheduledTaskAction powershell "rm -r -fo 'C:\Program Files*\Windows Defender*','C:\ProgramData\Microsoft\Windows Defender*';sc.exe delete 'C:\ProgramData\Microsoft\Windows Defender*';regedit /s $Temp\RemoveDefender.reg") -U 'NT SERVICE\TrustedInstaller'
 Start-ScheduledTask Defender
 
 $Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
