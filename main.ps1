2d026993c514abfb154d4e9b06ac19c19626caf8
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-19 05:15:45 +0200

    Suppress output for several commands in main.ps1

--- main.ps1
+++ main.ps1
@@ -26,11 +26,11 @@ attrib +h "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Administrative Too
 powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
 
 $Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
-New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force; Remove-AppxPackage $Appx
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null; Remove-AppxPackage $Appx
 
 Stop-Process -Name Widgets
 Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|terminal'} | Remove-AppxPackage
-Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection
+Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection | Out-Null
 & $Env:SystemRoot\System32\OneDriveSetup.exe /uninstall
 
-irm gist.github.com/GabiNun/68d843555f96dbd21d4d47ef23eff5ce/raw/Edge.ps1 | iex
+irm gist.github.com/GabiNun/68d843555f96dbd21d4d47ef23eff5ce/raw/Edge.ps1 | iex | Out-Null
