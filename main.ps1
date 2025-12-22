700a4d81f36d89b4c3e3ec124c562973cd9fe59d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-22 22:31:09 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,20 +1,14 @@
 $ProgressPreference = 'SilentlyContinue'
 
-irm github.com/GabiNun/Scripts/raw/main/Script.reg -Out Script.reg
-regedit /s Script.reg
-
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility"
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
 
 powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
 
-$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
-New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null; Remove-AppxPackage $Appx
+irm github.com/GabiNun/Scripts/raw/main/Script.reg -Out Script.reg
+regedit /s Script.reg
 
-Stop-Process -Name Widgets
-Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|terminal'} | Remove-AppxPackage
-Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection | Out-Null
-& $Env:SystemRoot\System32\OneDriveSetup /uninstall
+irm github.com/GabiNun/Scripts/raw/main/Packages.ps1 | iex | Out-Null
 
 irm gist.github.com/GabiNun/68d843555f96dbd21d4d47ef23eff5ce/raw/Edge.ps1 | iex | Out-Null
