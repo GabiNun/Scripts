a5e18fbe45d04d4e7cdfcf4bb62533546dfd92ce
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-22 22:29:45 +0200

    Create Packages.ps1

---
+++ Packages.ps1
@@ -0,0 +1,7 @@
+$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:USERNAME).Sid.Value
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null; Remove-AppxPackage $Appx
+
+Stop-Process -Name Widgets
+Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|terminal'} | Remove-AppxPackage
+Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection | Out-Null
+& $Env:SystemRoot\System32\OneDriveSetup /uninstall
