f01d0205bbf74ac3d990b3b4e12fe5a6fd55ae1c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-27 23:19:22 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,16 +1,6 @@
 $ProgressPreference = 'SilentlyContinue'
 
-attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility"
-attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
-attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
-
-powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
-
+irm github.com/GabiNun/Scripts/raw/main/Settings.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Script.reg -Out Script.reg;regedit /s Script.reg
-
 irm github.com/GabiNun/Scripts/raw/main/Packages.ps1 | iex | Out-Null
-
 irm gist.github.com/GabiNun/68d843555f96dbd21d4d47ef23eff5ce/raw/Edge.ps1 | iex | Out-Null
-
-irm pastebin.com/raw/vfJ061e2 | iex | Out-Null
-irm pastebin.com/raw/wxwgM97h | iex | Out-Null
