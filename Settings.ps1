226ff41056b4a91f7a42452ab1618881327da64c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-28 08:20:09 +0200

    Update Settings.ps1

--- Settings.ps1
+++ Settings.ps1
@@ -2,32 +2,32 @@ winget source remove msstore
 winget install glazewm
 winget remove zebar
  
-New-Item "$Home\.glzr\glazewm\config.yaml" -Value (irm 'https://pastebin.com/raw/zGgVsPFm') -Force
+New-Item .glzr\glazewm\config.yaml -Value (irm 'https://pastebin.com/raw/zGgVsPFm') -Force
 
 Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GlazeWM.lnk"
 Remove-Item "C:\Program Files (x86)\Microsoft.NET" -Recurse
 Remove-Item "C:\ProgramData\Microsoft OneDrive" -Recurse
-Remove-Item "$Env:AppData\Adobe" -Recurse
-Remove-Item "$Env:OneDrive" -Recurse -Force
-Remove-Item "$Home\Script.reg" -Force
-Remove-Item "C:\Windows.old"
+Remove-Item $Env:OneDrive -Recurse -Force
+Remove-Item $Env:AppData\Adobe -Recurse
+Remove-Item C:\Windows.old
+Remove-Item Script.reg
 
 attrib +h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk"
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
 attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility"
-attrib +h "$Env:Public"
-attrib +h "$Home\.glzr"
-attrib +h "$Home\Videos"
-attrib +h "$Home\Searches"
-attrib +h "$Home\Saved Games"
-attrib +h "$Home\Pictures"
-attrib +h "$Home\Music"
-attrib +h "$Home\Links"
-attrib +h "$Home\Favorites"
-attrib +h "$Home\Documents"
-attrib +h "$Home\Contacts"
-attrib -h "$Home\AppData"
-attrib +h "C:\inetpub"
+attrib +h "Saved Games"
+attrib +h $Env:Public
+attrib +h C:\inetpub
+attrib +h .glzr
+attrib +h Videos
+attrib +h Searches
+attrib +h Pictures
+attrib +h Music
+attrib +h Links
+attrib +h Favorites
+attrib +h Documents
+attrib +h Contacts
+attrib -h AppData
 
 powercfg /setactive SCHEME_MIN;powercfg /change monitor-timeout-ac 60;powercfg /h off
