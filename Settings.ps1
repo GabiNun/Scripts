b8ac83d5bbeb6722d9c5856be563bfa4a6d3c51b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-30 21:46:50 +0200

    Update Settings.ps1

--- Settings.ps1
+++ Settings.ps1
@@ -2,7 +2,7 @@ winget source remove msstore
 winget install glazewm
 winget remove zebar
  
-New-Item .glzr\glazewm\config.yaml -Value (irm 'https://pastebin.com/raw/zGgVsPFm') -Force -Attributes Hidden
+New-Item .glzr\glazewm\config.yaml -Value (irm 'https://pastebin.com/raw/zGgVsPFm') -Force
 
 Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\GlazeWM.lnk"
 Remove-Item "C:\Program Files (x86)\Microsoft.NET" -Recurse
@@ -19,6 +19,7 @@ attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility"
 attrib +h "Saved Games"
 attrib +h $Env:Public
 attrib +h C:\inetpub
+attrib +h .glzr
 attrib +h Videos
 attrib +h Searches
 attrib +h Pictures
