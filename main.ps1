a9264fc05d5b3f681268d52c0ad546b808ab362a
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-01 10:09:56 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,3 @@
-$ProgressPreference = 'SilentlyContinue'
-
 irm github.com/GabiNun/Scripts/raw/main/Settings.reg -Out Script.reg;regedit /s Script.reg;Stop-Process -Name explorer
 irm github.com/GabiNun/Scripts/raw/main/Settings.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Packages.ps1 | iex | Out-Null
