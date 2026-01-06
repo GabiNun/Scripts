b1a016eebcd34ec1264c5957a9045de12edb87f8
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-06 23:33:48 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,3 +1,4 @@
 irm github.com/GabiNun/Scripts/raw/main/Settings.reg -Out Script.reg;regedit /s Script.reg;Stop-Process -Name explorer;Remove-Item Script.reg
 irm github.com/GabiNun/Scripts/raw/main/Settings.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Packages.ps1 | iex | Out-Null
+irm https://gist.github.com/GabiNun/9a55a4317a33916c8d40e9a8b7c360ea/raw/Edge.ps1 | iex | Out-Null
