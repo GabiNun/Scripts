96b9187362067988cb84d3973600320f2f7a5036
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-06 07:34:03 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,3 @@
-$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:UserName).Sid.Value
-
 irm github.com/GabiNun/Scripts/raw/main/Settings.reg -Out Script.reg;regedit /s Script.reg;Stop-Process -Name explorer;Remove-Item Script.reg
 irm github.com/GabiNun/Scripts/raw/main/Settings.ps1 | iex | Out-Null
 irm github.com/GabiNun/Scripts/raw/main/Packages.ps1 | iex | Out-Null
