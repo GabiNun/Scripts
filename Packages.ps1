a242eca123718c34067829086fa61644aa74f00a
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-06 05:17:59 +0200

    Update Packages.ps1

--- Packages.ps1
+++ Packages.ps1
@@ -4,4 +4,4 @@ New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\E
 Stop-Process -Name Widgets -ErrorAction SilentlyContinue
 Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|terminal'} | Remove-AppxPackage -AllUsers
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection -NoRestart
-& $Env:SystemRoot\System32\OneDriveSetup /uninstall
+C:\Windows\System32\OneDriveSetup /uninstall
