521746ace759e8851fdc285b7216c28916bef151
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-06 23:33:12 +0200

    Update Packages.ps1

--- Packages.ps1
+++ Packages.ps1
@@ -1,9 +1,6 @@
 $Appx = (Get-AppxPackage *SecHealthUI).PackageFullName;$Sid = (glu $Env:UserName).Sid.Value
 New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force;Remove-AppxPackage $Appx
 
-$Appx = (Get-AppxPackage *EdgeDevToolsClient).PackageFullName
-New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force;Remove-AppxPackage $Appx
-
 Stop-Process -Name Widgets -ErrorAction Ignore
 Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatch 'Notepad|terminal'} | Remove-AppxPackage -AllUsers
 Disable-WindowsOptionalFeature -O -F Microsoft-RemoteDesktopConnection -NoRestart
@@ -11,10 +8,3 @@ C:\Windows\System32\OneDriveSetup /uninstall
 
 New-Item "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force
 & "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" --uninstall --system-level --force-uninstall --delete-profile
-
-Get-Process *Edge*,SearchHost | Stop-Process -Force
-Remove-Item "$Env:ProgramFiles (x86)\Microsoft" -Recurse -Force
-Remove-Item C:\ProgramData\Microsoft\EdgeUpdate -Recurse -Force
-sc.exe delete edgeupdate
-sc.exe delete edgeupdatem
-Unregister-ScheduledTask *Edge* -Confirm:$False
